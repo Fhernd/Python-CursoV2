72ee1658e78a3d872f90d2d92539c1b3d6e20408
infzer0 <johnortizo@outlook.com>
2020-02-08 17:12:17 -0500

    Python - Curso V2 - Creación Archivo demo.py
    Creación de archivo Python de demostración.

---
+++ etc/demo.py
@@ -0,0 +1,10 @@
+import math
+
+# Muestra el valor de la constante Euler:
+print(math.e)
+
+# Muestra el valor de la constante Pi:
+print(math.pi)
+
+# Calcula el seno de Pi:
+print(math.sin(math.pi))
