15d37c8204b23cefbfa9a8bc0eb92ba06522d74d
infzer0 <johnortizo@outlook.com>
2020-02-10 12:27:02 -0500

    Python - Curso V2 - Demostración de IDLE
    Python Curso V2: 12 Demostración de IDLE como Editor de Código Fuente Python sobre Archivos.

---
+++ etc/demo_idle.py
@@ -0,0 +1,17 @@
+print(2 + 2)
+print('Python v. 3.8.1')
+
+import math
+
+print(math.e)
+print(math.pi)
+print(math.sin(math.pi))
+
+numeros = [2, 3, 5, 7, 11, 13, 17]
+print(len(numeros))
+print(sum(numeros))
+print(min(numeros))
+print(max(numeros))
+
+for n in numeros:
+    print(n)
