e981fffacd6db612d70834925b7ad3192d2bb82f
infzer0 <johnortizo@outlook.com>
2020-02-12 17:40:50 -0500

    Python - Curso V2 - Tipo de Dato de una Variable
    Python Curso V2: 18 Consultando el Tipo de Dato y el Identificador Único de una Variable.

--- parte04/4.3_variables.py
+++ parte04/4.3_variables.py
@@ -3,3 +3,16 @@ dias_vividos = edad * 365
 
 print(edad)
 print(dias_vividos)
+
+print('Tipo de dato de la variable edad:', type(edad))
+print('Tipo de dato de la variable dias_vividos:', type(dias_vividos))
+
+print()
+
+print('Posición en memoria de la variable edad:', id(edad))
+print('Posición en memoria de la variable dias_vividos:', id(dias_vividos))
+
+print()
+
+print('Posición en memoria del valor 29:', id(29))
+print('Posición en memoria del valor 10585:', id(10585))
