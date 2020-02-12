60a8951457cc8ca60f0017b861ad9bd6ebe7a4b6
infzer0 <johnortizo@outlook.com>
2020-02-12 17:54:11 -0500

    Python - Curso V2 - Variables de Tipo Texto
    Python Curso V2: Estudio y Comprensión de las Variables para Guardar y Representar Texto.

---
+++ parte04/4.4_variables_cadenas_caracteres.py
@@ -0,0 +1,13 @@
+nombre = 'Edward'
+apellido = "Ortiz"
+nombre_completo = nombre + ' ' + apellido
+
+print('Nombre:', nombre)
+print('Apellido:', apellido)
+print('Nombre completo:', nombre_completo)
+
+print()
+
+print('Tipo de dato de la variable nombre:', type(nombre))
+print('Tipo de dato de la variable apellido:', type(apellido))
+print('Tipo de dato de la variable nombre_completo:', type(nombre_completo))
