81ee628376cfb818d242ab5acabfee7eab1ea88f
infzer0 <johnortizo@outlook.com>
2020-02-13 12:31:11 -0500

    Python - Curso V2 - Ejercicio 4.4
    Python Curso V2: 24 Ejercicio 4.4 - Invertir el Orden de los Caracteres del Nombre de una Persona.

---
+++ parte04/ex4.4_invertir_nombre.py
@@ -0,0 +1,11 @@
+'''
+Ejercicio 4.4: Pedir el nombre al usuario y obtener su representación en orden inverso.
+'''
+
+nombre = input('Digite su nombre: ')
+
+print('Su nombre es:', nombre)
+
+nombre_invertido = nombre[::-1]
+
+print('Su nombre invertido es:', nombre_invertido)
