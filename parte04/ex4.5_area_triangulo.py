764a696db4bcefe8a6ecf835a6f4c3119cbcb6e6
infzer0 <johnortizo@outlook.com>
2020-02-13 12:44:41 -0500

    Python - Curso V2 - Solución Ejercicio 4.5
    Python Curso V2: 25 Ejercicio 4.5 - Calcular el Área de un Triángulo Dadas la Base y la Altura.

---
+++ parte04/ex4.5_area_triangulo.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 4.5: Calcular el área de un triángulo dadas la base y la altura.
+'''
+
+base = float(input('Digite la base del triángulo: '))
+altura = float(input('Digite la altura del triángulo: '))
+
+area = base * altura / 2
+
+print('El área del triángulo es igual a {}.'.format(area))
