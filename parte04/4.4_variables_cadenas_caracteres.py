dfcf2332b2d8ce8d1f656e516202708f6ee8dcfe
infzer0 <johnortizo@outlook.com>
2020-02-13 08:38:21 -0500

    Python - Curso V2 - Concatenación Datos Numéricos y Textuales
    Python Curso V2: 20 Mecanismos de Concatenación de Variables Enteras y Cadenas de Caracteres.

--- parte04/4.4_variables_cadenas_caracteres.py
+++ parte04/4.4_variables_cadenas_caracteres.py
@@ -11,3 +11,15 @@ print()
 print('Tipo de dato de la variable nombre:', type(nombre))
 print('Tipo de dato de la variable apellido:', type(apellido))
 print('Tipo de dato de la variable nombre_completo:', type(nombre_completo))
+
+print()
+
+edad = 29
+
+resultado = nombre_completo + ' tiene ' + str(edad) + ' años.'
+print(resultado)
+
+print()
+
+resultado = '{} tiene {} años.'.format(nombre_completo, edad)
+print(resultado)
