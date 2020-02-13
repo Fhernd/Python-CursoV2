5a338a3aa65f744fa4aa09720f5a5d733950bea0
infzer0 <johnortizo@outlook.com>
2020-02-13 08:57:00 -0500

    Python - Curso V2 - Presentación de Ejercicios a Desarrollar
    Python Curso V2: 21 Ejercicio 4.1 - Versión Actual de Python Instalada en el Sistema.

---
+++ parte04/ex4.1_version_python.py
@@ -0,0 +1,11 @@
+'''
+Ejercicio 4.1: Mostrar en pantalla la versión actual de Python instalada en el sistema.
+'''
+
+import sys
+
+print('Versión de Python:', sys.version)
+
+print()
+
+print('Información de versión:', sys.version_info)
