e96d90cac49c8c62b6493cae3a1c9c8b134d9831
infzer0 <johnortizo@outlook.com>
2020-02-16 17:07:24 -0500

    Python - Curso V2 - Tipo de Dato complex
    Python Curso V2: 36 El Tipo de Dato Complejo (complex) para Representar Números Complejos.

---
+++ parte05/5.03_complejos_complex.py
@@ -0,0 +1,11 @@
+# Números complejos:
+
+numero_complejo = 2 - 3j
+print('Contenido variable compleja:', numero_complejo)
+print('El tipo de dato de la variable numero_complejo es:', type(numero_complejo))
+
+print()
+
+numero_complejo = complex(2, -3)
+print('Contenido variable compleja:', numero_complejo)
+print('El tipo de dato de la variable numero_complejo es:', type(numero_complejo))
