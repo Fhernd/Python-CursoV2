7a43a0cb27f0e530569d45d23eec157eaf5f553c
infzer0 <johnortizo@outlook.com>
2020-02-16 15:49:24 -0500

    Python - Curso V2 - Tipo de Dato float
    Python Curso V2: 33 El Tipo de Dato Punto Flotante (float) para Representar Cantidad Numéricas Reales.

---
+++ parte05/5.02_reales_float.py
@@ -0,0 +1,14 @@
+# Números de punto flotante:
+
+pi = 3.1415
+precio = 29.95
+
+print('El valor de la variable pi es: %.4f' % pi)
+print('El valor de la variable precio es: %.2f' % precio)
+print(pi)
+print(precio)
+
+print()
+
+print('El tipo de dato para la variable pi: ', type(pi))
+print('El tipo de dato para la variable precio: ', type(precio))
