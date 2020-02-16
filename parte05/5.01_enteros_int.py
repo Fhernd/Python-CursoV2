ae645211eadf5b44d96df048732dc79ebcfd6bec
infzer0 <johnortizo@outlook.com>
2020-02-16 10:56:31 -0500

    Python - Curso V2 - Operaciones Tipo de Dato Entero
    Python Curso V2: 30 Operaciones Básicas con Variables de Tipo de Dato Simple Entero (int).

--- parte05/5.01_enteros_int.py
+++ parte05/5.01_enteros_int.py
@@ -5,3 +5,16 @@ x = -5
 
 print(type(puntaje))
 print(type(x))
+
+print()
+print('Puntaje antes de la adición 50 puntos: %i' % puntaje)
+puntaje = puntaje + 50 # puntaje += 50
+print('Puntaje después de la adición 50 puntos: %i' % puntaje)
+print(type(puntaje))
+
+print()
+
+print('Valor de x antes de la adición: %i' % x)
+x += 10 # x = x + 10
+print('Valor de x después de la adición: %i' % x)
+print('Tipo de dato de la variable `x`: %s' % type(x))
