df69a5b4fee57a909d950156d2fcbbd92f2611f6
infzer0 <johnortizo@outlook.com>
2020-02-16 17:19:09 -0500

    Python - Curso V2 - Operaciones con Números Complejos
    Python Curso V2: 37 Operaciones Básicas con Variables de Tipo de Dato Simple Complejo.

--- parte05/5.03_complejos_complex.py
+++ parte05/5.03_complejos_complex.py
@@ -9,3 +9,19 @@ print()
 numero_complejo = complex(2, -3)
 print('Contenido variable compleja:', numero_complejo)
 print('El tipo de dato de la variable numero_complejo es:', type(numero_complejo))
+
+print()
+
+print('Operaciones aritméticas sobre números complejos:')
+
+suma = 2 - 3j + (1 + 5j)
+print('Suma:', suma)
+
+resta = 2 - 3j - complex(1, 5)
+print('Resta:', resta)
+
+producto = 2 - 3j * complex(1, 5)
+print('Producto:', producto)
+
+division = 2 - 3j / complex(1, 5)
+print('División:', division)
