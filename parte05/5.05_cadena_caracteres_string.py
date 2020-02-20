a25be6ed1bba2723da0d73cfa6c8886dbb1b5eb5
infzer0 <johnortizo@outlook.com>
2020-02-20 12:32:39 -0500

    Python - Curso V2 - Tipo de Datos str
    Python Curso V2: 44 El Tipo de Dato str para Representar Cadenas de Caracteres.

---
+++ parte05/5.05_cadena_caracteres_string.py
@@ -0,0 +1,17 @@
+# Tipo de dato compuesto string
+
+nombre = 'Edward Ortiz'
+email = "edward@mail.co"
+direccion = '''Carrera 1 # 13A-29
+Bogotá - Colombia
+GMT-05
+'''
+
+print(type(nombre))
+print(nombre)
+
+print()
+
+mensaje = 'Bienvenido(a), ' + nombre + '. Correo: ' + email
+print(type(mensaje))
+print(mensaje)
