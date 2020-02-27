052a2187df11eac6ab5a1d91ad2e96f4801d3a40
infzer0 <johnortizo@outlook.com>
2020-02-27 11:57:39 -0500

    Python - Curso V2 - Tipo de Dato Tupla
    Python - Curso V2: 51 El Tipo de Dato Tupla (tuple) para Representar un Contenedor de Datos.

---
+++ parte05/5.06_tuplas_tuple.py
@@ -0,0 +1,6 @@
+# Tipo de dato compuesto - tupla - estático:
+
+punto = (2, 5)
+print('Tipo de dato:', type(punto))
+print('Contenido de la variable punto:', punto)
+print('Cantidad elementos de la tupla punto:', len(punto))
