ec6846daf0455bd69db6df338f6955198f8563a8
infzer0 <johnortizo@outlook.com>
2020-02-27 12:12:39 -0500

    Python - Curso V2 - Acceso a los Elementos de una Tupla
    Python - Curso V2: 52 Acceso a los Elementos (Datos) de un Objeto de Tipo de Dato Tupla.

--- parte05/5.06_tuplas_tuple.py
+++ parte05/5.06_tuplas_tuple.py
@@ -4,3 +4,27 @@ punto = (2, 5)
 print('Tipo de dato:', type(punto))
 print('Contenido de la variable punto:', punto)
 print('Cantidad elementos de la tupla punto:', len(punto))
+
+print()
+
+# Acceso a los elementos de una tupla:
+x = punto[0]
+y = punto[1]
+print('El valor en x es igual a:', x)
+print('El valor en y es igual a:', y)
+
+print()
+
+# Desempaquetamiento:
+abscisa, ordenada = punto
+print('El valor en x es igual a:', abscisa)
+print('El valor en y es igual a:', ordenada)
+
+print()
+
+# Acceso con índices negativos (de derecha a izquierda):
+
+penultimo_elemento = punto[-2]
+ultimo_elemento = punto[-1]
+print('El valor en x es igual a:', penultimo_elemento)
+print('El valor en y es igual a:', ultimo_elemento)
