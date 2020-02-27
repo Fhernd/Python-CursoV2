646b562b125c43ec7cca56c6cfc123fb2f4e18ab
infzer0 <johnortizo@outlook.com>
2020-02-27 15:56:49 -0500

    Python - Curso V2 - Tipo de Dato Lista
    Python - Curso V2: 58 El Tipo de Dato Dinámico (Mutable) Lista como Contenedor de Datos.

---
+++ parte05/5.07_lista_list.py
@@ -0,0 +1,8 @@
+# Tipo de dato compuesto: lista
+# Las listas son un tipo de dato dinámico
+
+# 1. Creación de una lista:
+numeros = [2, 4, 6, 8, 10]
+print('Tipo de dato de la variable numeros:', type(numeros))
+print('Cantidad de elementos:', len(numeros))
+print('Contenido:', numeros)
