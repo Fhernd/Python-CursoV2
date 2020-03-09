786b48b2758c264c8d8b4c773ea0968c7b45f62c
infzer0 <johnortizo@outlook.com>
2020-03-09 16:30:27 -0500

    Python - Curso V2 - Creación Diccionarios
    Python Curso V2: 74 Mecanismos Alternativos para la Creación de un Objeto Diccionario.

---
+++ parte05/5.08_diccionario_dict.py
@@ -0,0 +1,14 @@
+# Tipo de dato compuesto: diccionario
+
+# 1. Creación de diccionarios:
+diccionario_1 = {'propiedad1': 1, 'propiedad2': 2, 'propiedad3': [1, 2, 3]}
+diccionario_2 = dict()
+diccionario_2['propiedad1'] = 1
+diccionario_2['propiedad2'] = 2
+diccionario_2['propiedad3'] = [1, 2, 3]
+
+print(diccionario_1)
+print(type(diccionario_1))
+print()
+print(diccionario_2)
+print(type(diccionario_2))
