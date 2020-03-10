12b19e6e6c8206d6fca4b7e0365e6dda72f51e63
infzer0 <johnortizo@outlook.com>
2020-03-10 08:11:21 -0500

    Python - Curso V2 - Creación Conjunto desde una Cadena de Caracteres
    Python Curso V2: 88 Crear un Conjunto (set) a partir de una Cadena de Caracteres (str).

--- parte05/5.09_conjunto_set.py
+++ parte05/5.09_conjunto_set.py
@@ -13,3 +13,14 @@ print()
 print('Contenido de la variable `conjunto_2`:', conjunto_2)
 print('Cantidad de elementos del conjunto `conjunto_2`:', len(conjunto_2))
 print('El tipo de dato de la variable `conjunto_2` es:', type(conjunto_2))
+
+print()
+
+# 1.2 Creación de conjuntos por medio de cadenas de caracteres:
+print('1.2 Creación de conjuntos por medio de cadenas de caracteres:')
+frase = 'Python es un lenguaje de programación'
+caracteres = set(frase)
+print('Contenido de la variable conjunto `caracteres`:', caracteres)
+print('El tipo de dato de la variable `caracteres` es:', type(caracteres))
+print('Cantidad de elementos del conjunto `caracteres`:', len(caracteres))
+print('Cantidad de caracteres de la variable string `frase`:', len(frase))
