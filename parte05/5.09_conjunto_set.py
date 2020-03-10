7538a78eaddf808878fd4826e8640731b426e590
infzer0 <johnortizo@outlook.com>
2020-03-10 08:00:23 -0500

    Python - Curso V2 - Tipo de Dato Conjunto (set)
    Python Curso V2: 87 Múltiples Mecanismos para Crear una Variable de Tipo de Dato set (Conjunto).

---
+++ parte05/5.09_conjunto_set.py
@@ -0,0 +1,15 @@
+# Tipo de dato compuesto mutable y dinámico: set (conjunto)
+
+# 1. Creación de un conjunto:
+print('1. Creación de un conjunto:')
+
+conjunto_1 = {'Juan', 'Oliva', 'Edward', 'Daniela', 'Juan', 'Juan', 'Germán'}
+conjunto_2 = set(['Juan', 'Oliva', 'Edward', 'Daniela', 'Juan', 'Juan', 'Germán'])
+
+print('Contenido de la variable `conjunto_1`:', conjunto_1)
+print('Cantidad de elementos del conjunto `conjunto_1`:', len(conjunto_1))
+print('El tipo de dato de la variable `conjunto_1` es:', type(conjunto_1))
+print()
+print('Contenido de la variable `conjunto_2`:', conjunto_2)
+print('Cantidad de elementos del conjunto `conjunto_2`:', len(conjunto_2))
+print('El tipo de dato de la variable `conjunto_2` es:', type(conjunto_2))
