a10928f32508113a13233f50dcb118e2394caa87
infzer0 <johnortizo@outlook.com>
2020-03-10 08:21:35 -0500

    Python - Curso V2 - Creación Conjunto desde una Tupla
    Python Curso V2: 89 Crear un Conjunto (set) a partir de una Tupla (tupla).

--- parte05/5.09_conjunto_set.py
+++ parte05/5.09_conjunto_set.py
@@ -24,3 +24,20 @@ print('Contenido de la variable conjunto `caracteres`:', caracteres)
 print('El tipo de dato de la variable `caracteres` es:', type(caracteres))
 print('Cantidad de elementos del conjunto `caracteres`:', len(caracteres))
 print('Cantidad de caracteres de la variable string `frase`:', len(frase))
+
+print()
+
+# 1.3 Creación de un conjunto por medio de una tupla (tuple):
+print('1.3 Creación de un conjunto por medio de una tupla (tuple):')
+
+primos = (7, 3, 5, 2, 7, 5, 13, 11, 19, 2, 2, 2, 5, 5)
+print('Contenido de la variable `primos`:', primos)
+print('Cantidad de elementos de la variable `primos`:', len(primos))
+print('Tipo de dato de la variable tupla `primos`:', type(primos))
+
+print()
+
+primos_unicos = set(primos)
+print('Contenido de la variable `primos_unicos`:', primos_unicos)
+print('Cantidad de elementos de la variable `primos_unicos`:', len(primos_unicos))
+print('Tipo de dato de la variable tupla `primos_unicos`:', type(primos_unicos))
