03fd96eeefd2d911f3afd227bcdb5afa544b319a
infzer0 <johnortizo@outlook.com>
2020-03-11 11:30:05 -0500

    Python - Curso V2 - Ejercicio 5.9
    Python Curso V2: 112 Ejercicio 5.9 Sumar Todos los Valores de un Diccionario.

---
+++ parte05/ex5.09_suma_valores_diccionario.py
@@ -0,0 +1,7 @@
+# Ejercicio 5.9: Sumar todos los valores de un diccionario.
+
+productos = {'Mouse': 29.9, 'Teclado': 119.9, 'Audífonos': 35.9, 'Monitor': 299}
+
+total = sum(productos.values())
+
+print('El total de los productos es:', total)
