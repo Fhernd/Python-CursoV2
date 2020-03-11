d786e6810c1908fd0bc1380a66805a582c09135d
infzer0 <johnortizo@outlook.com>
2020-03-11 08:59:43 -0500

    Python - Curso V2 - Ejercicio 5.3
    Python Curso V2: 106 Ejercicio 5.3 Crear una Sección de una Tupla con Notación de Rebanado (Slicing).

---
+++ parte05/ex5.03_seccion_tupla.py
@@ -0,0 +1,12 @@
+# Ejercicio 5.3: Crear una sección de una tupla usando notación de slicing (rebanado).
+
+# (1, 2, 3, 4, 5)
+
+numeros = (1, 2, 3, 4, 5)
+print(numeros)
+
+print()
+
+seccion = numeros[1:4]
+
+print(seccion)
