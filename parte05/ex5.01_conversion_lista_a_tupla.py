d415fa20725af57f531dccf67425ec16f46860f7
infzer0 <johnortizo@outlook.com>
2020-03-10 19:42:51 -0500

    Python - Curso V2 - Ejercicio 5.1
    Python Curso V2: 104 Ejercicio 5.1 Convertir una Lista en una Tupla.

---
+++ parte05/ex5.01_conversion_lista_a_tupla.py
@@ -0,0 +1,12 @@
+# Ejercicio 5.1: Convertir una lista en una tupla.
+
+lenguajes = ['Python', 'JavaScript', 'PHP', 'C#', 'C++', 'Java']
+
+lenguajes_tupla = tuple(lenguajes)
+
+print(lenguajes)
+
+print()
+
+print(lenguajes_tupla)
+print(type(lenguajes_tupla))
