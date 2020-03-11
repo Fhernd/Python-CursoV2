8d129241e907549e2585656eb172eff2e850f24e
infzer0 <johnortizo@outlook.com>
2020-03-11 18:14:11 -0500

    Python - Curso V2 - Operadores Aritméticos Suma
    Python Curso V2: 120 Operador Numérico Suma (+) para Sumar Valores Enteros.

---
+++ parte06/6.01_operadores_numericos.py
@@ -0,0 +1,20 @@
+# Operadores numéricos:
+
+# Operador suma: +
+
+numero_1 = 2
+numero_2 = 3
+
+suma = numero_1 + numero_2
+
+print(f'La suma de {numero_1} y {numero_2} es igual a {suma}')
+
+print()
+
+suma = 2 + 3
+print(f'La suma de {2} y {3} es igual a {suma}')
+
+print()
+
+suma = numero_1 + 3
+print(f'La suma de {numero_1} y {3} es igual a {suma}')
