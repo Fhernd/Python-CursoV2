94475a776bd577112459bc718cc76966940a1e4f
infzer0 <johnortizo@outlook.com>
2020-03-11 14:11:36 -0500

    Python - Curso V2 - Ejercicio 5.15
    Python Curso V2: 118 Ejercicio 5.15 Diferencia Simétrica entre Dos Conjuntos de Programas.

---
+++ parte05/ex5.15_diferencia_simetrica_conjuntos.py
@@ -0,0 +1,8 @@
+# Ejercicio 5.15: Calcular la diferencia simétrica entre dos conjuntos.
+
+escritorio = {'Notepad++', 'Atom', 'Eclipse', 'NetBeans', 'PyCharm'}
+portatil = {'Notepad++', 'PyCharm', 'Visual Studio Code', 'IntelliJ IDEA'}
+
+resultado = escritorio.symmetric_difference(portatil)
+
+print('Contenido del conjunto `resultado`:', resultado)
