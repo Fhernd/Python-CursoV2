d248fcf2236cbe6b0c28337e4a87af6dab66bf21
infzer0 <johnortizo@outlook.com>
2020-03-12 06:58:19 -0500

    Python - Curso V2 - Suma de Reales
    Python Curso V2: 121 Suma de N Cantidad de Elementos y Suma de Valores Reales (Punto Flotante).

--- parte06/6.01_operadores_numericos.py
+++ parte06/6.01_operadores_numericos.py
@@ -18,3 +18,24 @@ print()
 
 suma = numero_1 + 3
 print(f'La suma de {numero_1} y {3} es igual a {suma}')
+
+print()
+# Suma de n cantidad de elementos:
+print('Suma de n cantidad de elementos:')
+suma = 1 + 2 + 3 + 4 + 5
+# 1 + 2 = 3
+# 3 + 3 = 6
+# 6 + 4 = 10
+# 10 + 5 = 15
+
+print('La suma de los números de 1 a 5 es igual a:', suma)
+
+print()
+
+# Suma de números reales (punto flotante):
+print('Suma de números reales (punto flotante):')
+numero_1 = 3.1415
+numero_2 = 2.7172
+
+suma = numero_1 + numero_2
+print(f'La suma de {numero_1} y {numero_2} es igual {suma}.')
