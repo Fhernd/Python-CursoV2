1d21377692b99c648a26ae723dbd31b9f6348ce8
infzer0 <johnortizo@outlook.com>
2020-03-12 11:10:29 -0500

    Python - Curso V2 - Operador de Comparación Igualdad
    Python Curso V2: 134 Operador de Comparación de Igualdad ==.

---
+++ parte06/6.02_operadores_comparacion.py
@@ -0,0 +1,21 @@
+# Operadores de comparación.
+
+# 1. Igual: ==
+
+programa_1 = 'Visual Studio Code'
+programa_2 = 'visual studio Code'
+
+print("¿'{}' es igual a '{}'?: {}".format(programa_1, programa_2, programa_1 == programa_2))
+
+programa_3 = 'Visual Studio Code'
+print("¿'{}' es igual a '{}'?: {}".format(programa_1, programa_3, programa_1 == programa_3))
+
+print()
+
+numero_1 = 1
+numero_2 = 3
+
+print("¿{} es igual a {}?: {}".format(numero_1, numero_2, numero_1 == numero_2))
+
+numero_3 = 1
+print("¿{} es igual a {}?: {}".format(numero_1, numero_3, numero_1 == numero_3))
