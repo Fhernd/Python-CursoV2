f405b40ae16a7c343b58c2660d1d67d5cdba3d23
infzer0 <johnortizo@outlook.com>
2020-03-13 10:28:10 -0500

    Python - Curso V2 - Operador a Nivel de bits (Bitwise) and
    Python Curso V2: 145 Operador a Nivel de Bits (Bitwise) and.

---
+++ parte06/6.04_operadores_nivel_bits.py
@@ -0,0 +1,16 @@
+# Operadores a nivel de bits (bitwise):
+print('Operadores a nivel de bits:')
+
+# Operador and: &
+
+numero_1 = 5 # 101
+numero_2 = 2 # 10
+
+# 101
+# 010 &
+# ====
+# 000
+
+resultado = numero_1 & numero_2
+
+print('numero_1 & numero_2 =', resultado)
