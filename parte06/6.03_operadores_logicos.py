0df2320beca317cf9840841c34fafd5c032ebcb4
infzer0 <johnortizo@outlook.com>
2020-03-13 09:51:30 -0500

    Python - Curso V2 - Operador Lógico Y and
    Python Curso V2: 142 Operador Lógico Y (and) para Componer Expresiones Booleanas.

---
+++ parte06/6.03_operadores_logicos.py
@@ -0,0 +1,13 @@
+# Operadores Lógicos
+
+# Operador lógico Y: and
+
+edad = 19
+nombre = 'Daniela'
+
+resultado = edad >= 18 and nombre == 'Daniela'
+print('¿La persona se llama Daniela y es mayor de edad?: {}'.format(resultado))
+
+edad = 17
+resultado = edad >= 18 and nombre == 'Daniela'
+print('¿La persona se llama Daniela y es mayor de edad?: {}'.format(resultado))
