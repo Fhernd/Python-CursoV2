f45036b4a321225e5e50f831c7f44e166be271fe
infzer0 <johnortizo@outlook.com>
2020-03-13 12:33:48 -0500

    Python - Curso V2 - Operadores de Asignación - Asignación, Incremento y Decremento
    Python Curso V2: 151 Operadores de Asignación - Asignación, Incremento y Decremento.

---
+++ parte06/6.05_operadores_asignacion.py
@@ -0,0 +1,22 @@
+# Operadores de Asignación:
+
+# Operador de asignación: =
+print('Operador de asignación: =')
+
+numero = 10
+
+print('Contenido de la variable `numero`:', numero)
+
+print()
+
+# Operador de incremento: +=
+print('Operador de incremento: +=')
+numero += 90
+print('Contenido de la variable `numero`:', numero)
+
+print()
+
+# Operador de decremento: -=
+
+numero -= 80
+print('Contenido de la variable `numero`:', numero)
