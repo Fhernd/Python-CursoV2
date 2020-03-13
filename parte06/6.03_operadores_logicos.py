752296484d3b67cd4669a87dfedc30f0ed080b67
infzer0 <johnortizo@outlook.com>
2020-03-13 10:01:32 -0500

    Python - Curso V2 - Operador Lógico O or
    Python Curso V2: 143 Operador Lógico O (or) para Componer Expresiones Booleanas.

--- parte06/6.03_operadores_logicos.py
+++ parte06/6.03_operadores_logicos.py
@@ -11,3 +11,19 @@ print('¿La persona se llama Daniela y es mayor de edad?: {}'.format(resultado))
 edad = 17
 resultado = edad >= 18 and nombre == 'Daniela'
 print('¿La persona se llama Daniela y es mayor de edad?: {}'.format(resultado))
+
+print()
+
+# Operador Lógico O: or
+
+edad = 23
+profesion = 'Programador'
+
+califica = edad > 20 or profesion == 'Programador'
+print('¿La persona tiene más de 20 años o es programadora?:', califica)
+
+edad = 17
+profesion = 'Ingeniero'
+
+califica = edad > 20 or profesion == 'Programador'
+print('¿La persona tiene más de 20 años o es programadora?:', califica)
