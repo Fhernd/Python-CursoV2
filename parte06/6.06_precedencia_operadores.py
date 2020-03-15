20ecec69d5810b4cc9f8b658e4ccc842538358e3
infzer0 <johnortizo@outlook.com>
2020-03-15 17:14:29 -0500

    Python - Curso V2 - Precedencia de Operadores
    Python Curso V2: 156 Aplicación de la Precedencia (Jerarquía) de Operadores en Expresión Aritméticas.

---
+++ parte06/6.06_precedencia_operadores.py
@@ -0,0 +1,14 @@
+# Precedencia (jerarquía) de operadores:
+
+resultado = 5/2 + 4
+print('El contenido de la variable `resultado` es:', resultado)
+
+print()
+
+resultado = 4 + 5/2
+print('El contenido de la variable `resultado` es:', resultado)
+
+print()
+
+resultado = 5/(2 + 4)
+print('El contenido de la variable `resultado` es:', resultado)
