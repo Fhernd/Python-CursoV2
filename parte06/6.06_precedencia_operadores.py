b2a442b25e05edc62d4b28c3560521d5e9934190
infzer0 <johnortizo@outlook.com>
2020-03-15 17:27:10 -0500

    Python - Curso V2 - Precedencia Exponenciación
    Python Curso V2: 157 Precedencia entre las Partes de la Operación de la Exponenciación.

--- parte06/6.06_precedencia_operadores.py
+++ parte06/6.06_precedencia_operadores.py
@@ -1,5 +1,7 @@
 # Precedencia (jerarquía) de operadores:
 
+from math import sqrt
+
 resultado = 5/2 + 4
 print('El contenido de la variable `resultado` es:', resultado)
 
@@ -11,4 +13,19 @@ print('El contenido de la variable `resultado` es:', resultado)
 print()
 
 resultado = 5/(2 + 4)
+print('El contenido de la variable `resultado` es:', resultado)
+
+print()
+
+resultado = 5/(2 + 4) + sqrt(25)
+print('El contenido de la variable `resultado` es:', resultado)
+
+print()
+
+resultado = 25 ** 1 / 2
+print('El contenido de la variable `resultado` es:', resultado)
+
+print()
+
+resultado = 25 ** (1 / 2)
 print('El contenido de la variable `resultado` es:', resultado)
