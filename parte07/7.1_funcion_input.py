17f4b8d38c7cb9152c42bd238b76aa636d83bc9a
infzer0 <johnortizo@outlook.com>
2020-03-16 12:52:15 -0500

    Python - Curso V2 - Uso Función input()
    Python Curso V2: 172 Ejemplo de Uso de la Función input() para Capturar Datos desde el Teclado.

---
+++ parte07/7.1_funcion_input.py
@@ -0,0 +1,8 @@
+# Entrada de datos (input) - Función incorporada `input()`:
+
+print('Entrada de datos (input) - Función incorporada `input()`:')
+
+nombre = input('Digite su nombre: ')
+
+print('El tipo de dato de la variable `nombre` es:', type(nombre).__name__)
+print('El contenido de la variable `nombre` es:', nombre)
