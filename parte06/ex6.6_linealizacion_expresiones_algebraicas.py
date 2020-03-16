02494eacd9395d769d4591d645dcf95a896e3df0
infzer0 <johnortizo@outlook.com>
2020-03-16 11:26:10 -0500

    Python - Curso V2 - Ejercicio 6.6
    Python Curso V2: 169 Ejercicio 6.6 Linealización de Expresiones Algebraicas a Expresiones Algorítmicas.

---
+++ parte06/ex6.6_linealizacion_expresiones_algebraicas.py
@@ -0,0 +1,18 @@
+# Ejercicio 6. Convertir expresiones algebraicas a expresiones programáticas.
+
+a = 1
+b = 2
+c = 3
+d = 16
+
+resultado = a / (b*c) / d**(1/2)**3
+
+print('Resultado:', resultado)
+
+print()
+
+e = 7
+
+resultado = (a**3)**2 - b*c/(d*e)
+
+print('Resultado:', resultado)
