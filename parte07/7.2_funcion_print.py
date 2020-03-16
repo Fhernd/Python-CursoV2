5dbf558ef02a4d8c7d025c2b2ebee92fcc61911d
infzer0 <johnortizo@outlook.com>
2020-03-16 13:46:24 -0500

    Python - Curso V2 - Función print()
    Python Curso V2: 175 Ejemplo de Uso de la Función print() para la Salida de Datos().

---
+++ parte07/7.2_funcion_print.py
@@ -0,0 +1,12 @@
+# Salida de datos (output) - Función incorporada `print()`:
+
+nombre = 'John Ortiz Ordoñez'
+edad = 29
+
+print('Nombre: ' + nombre, end=' - ')
+print('Edad: ' + str(edad))
+
+print()
+
+print('Aprendiendo el uso de la', end=' ')
+print('función print()')
