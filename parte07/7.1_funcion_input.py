a9a45d2b421acb68bbb59ee4f8429f5ec1a3785c
infzer0 <johnortizo@outlook.com>
2020-03-16 13:03:34 -0500

    Python - Curso V2 - Control del Error EOFError de la Función input()
    Python Curso V2: 173 Control de la Excepción EOFError de la Función input().

--- parte07/7.1_funcion_input.py
+++ parte07/7.1_funcion_input.py
@@ -2,7 +2,13 @@
 
 print('Entrada de datos (input) - Función incorporada `input()`:')
 
-nombre = input('Digite su nombre: ')
+try:
+    nombre = input('Digite su nombre: ')
 
-print('El tipo de dato de la variable `nombre` es:', type(nombre).__name__)
-print('El contenido de la variable `nombre` es:', nombre)
+    print('El tipo de dato de la variable `nombre` es:', type(nombre).__name__)
+    print('El contenido de la variable `nombre` es:', nombre)
+except EOFError:
+    print('El usuario ha cancelado la introducción de datos.')
+    print('El nombre de la persona no se capturó.')
+
+print('El programa ha finalizado de forma satisfactoria.')
