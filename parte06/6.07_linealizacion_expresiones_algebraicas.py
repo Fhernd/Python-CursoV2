7ff7100ee4c1ac6d2b440540e287324350cec559
infzer0 <johnortizo@outlook.com>
2020-03-16 08:41:14 -0500

    Python - Curso V2 - Otras Linealizaciones
    Python Curso V2: 162 Linealizar Otras Expresiones Algebraicas en Modo Programático.

--- parte06/6.07_linealizacion_expresiones_algebraicas.py
+++ parte06/6.07_linealizacion_expresiones_algebraicas.py
@@ -11,3 +11,21 @@ x_2 = (-b - (b**2 - 4*a*c)**(1/2)) / (2*a)
 
 print('Solución 1 (x_1):', x_1)
 print('Solución 1 (x_2):', x_2)
+
+print()
+
+# Ejemplo 2. X^2 + 4YZ
+
+x = 1
+y = 2
+z = 3
+
+resultado = x**2 + 4*y*z
+print('Resultado:', resultado)
+
+print()
+
+# Ejemplo 3. 
+
+resultado = (x + y)/z + 3*x/5 + 4*y
+print('Resultado:', resultado)
