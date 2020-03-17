d22f4064fec278826cfe21f7bc93fe65d06b0dcb
infzer0 <johnortizo@outlook.com>
2020-03-17 12:44:57 -0500

    Python - Curso V2 - Múltiples Valores en Función print()
    Python Curso V2: 176 Imprimir Múltiples Valores con la Función Incorporada print().

--- parte07/7.2_funcion_print.py
+++ parte07/7.2_funcion_print.py
@@ -10,3 +10,22 @@ print()
 
 print('Aprendiendo el uso de la', end=' ')
 print('función print()')
+
+print()
+
+# Múltiples argumentos para la función `print()`:
+print('Múltiples argumentos para la función `print()`:')
+
+email = 'john@mail.co'
+
+print(nombre, edad, email)
+
+print(nombre, edad, email, sep='')
+print(nombre, edad, email, sep=' - ')
+
+print()
+
+lenguajes = ('Python', 'JavaScript', 'C++', 'C#', 'Java')
+
+print(lenguajes)
+print(lenguajes[0], lenguajes[1], lenguajes[2], lenguajes[3], lenguajes[4], sep=' - ')
