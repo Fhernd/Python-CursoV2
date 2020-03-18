f387c710e43f99eca4273010f02458b6d6034fe0
infzer0 <johnortizo@outlook.com>
2020-03-18 16:47:09 -0500

    Python - Curso V2 - Estrictamente Mayor
    Python Curso V2: 180 Comprobar si un Número es Estrictamente Mayor a Otro con un Condicional if.

---
+++ parte08/8.2_numero_mayor.py
@@ -0,0 +1,9 @@
+# Uso de condicionales para determinar si un número es mayor a otro:
+
+numero_1 = int(input('Escriba el primer número: '))
+numero_2 = int(input('Escriba el segundo número: '))
+
+if numero_1 > numero_2:
+    print('El número {} es mayor a {}.'.format(numero_1, numero_2))
+
+print('El programa ha finalizado.')
