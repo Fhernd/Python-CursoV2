c2bd8b318ada1a161e669140636ac10093d5f005
infzer0 <johnortizo@outlook.com>
2020-03-18 13:33:42 -0500

    Python - Curso V2 - Introducción Condicional if
    Python Curso V2: 179 Introducción a la Sentencia de Control de Flujo if Condicional.

---
+++ parte08/8.1_condicionales_intro.py
@@ -0,0 +1,26 @@
+# Introducción a los condicionales.
+
+# Estudiar la sentencia `if`:
+
+edad = 17
+
+if edad <= 17:
+    print('Ud. es menor de edad.')
+else:
+    print('Ud. es mayor de edad.')
+
+print()
+
+if edad < 18:
+    print('Ud. es menor de edad.')
+else:
+    print('Ud. es mayor de edad.')
+
+print()
+
+edad = 29
+
+if edad < 18:
+    print('Ud. es menor de edad.')
+else:
+    print('Ud. es mayor de edad.')
