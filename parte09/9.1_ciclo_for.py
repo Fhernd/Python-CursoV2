df9204db252e3856c8b085b71b6efd15e4b3e8da
infzer0 <johnortizo@outlook.com>
2020-03-20 08:20:39 -0500

    Python - Curso V2 - Motivación Uso Ciclos
    Python Curso V2: 196 Motivación para el Uso de Ciclos para Solucionar Algoritmos Iterativos.

---
+++ parte09/9.1_ciclo_for.py
@@ -0,0 +1,20 @@
+# Ciclos - for
+
+numeros = [1, 2, 3, 4, 5]
+
+total = 0
+
+total = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4]
+print('Total de la suma de los números de 1 a 5:', total)
+
+print()
+
+total = 0
+
+total += numeros[0]
+total += numeros[1]
+total += numeros[2]
+total += numeros[3]
+total += numeros[4]
+# total += numeros[999]
+print('Total de la suma de los números de 1 a 5:', total)
