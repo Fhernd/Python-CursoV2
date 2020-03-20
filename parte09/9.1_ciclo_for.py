6da3668e2e58387eb17ce2794c74481e486cb32d
infzer0 <johnortizo@outlook.com>
2020-03-20 08:32:40 -0500

    Python - Curso V2 - Primer Uso Ciclo for
    Python Curso V2: 197 Primer Uso del Ciclo for para Sumar el Contenido de una Lista (Colección).

--- parte09/9.1_ciclo_for.py
+++ parte09/9.1_ciclo_for.py
@@ -18,3 +18,33 @@ total += numeros[3]
 total += numeros[4]
 # total += numeros[999]
 print('Total de la suma de los números de 1 a 5:', total)
+
+print()
+
+numeros.append(6)
+numeros.append(7)
+numeros.append(8)
+numeros.append(9)
+numeros.append(10)
+
+print('Contenido actual de la lista `numeros`:', numeros)
+
+total += numeros[5]
+total += numeros[6]
+total += numeros[7]
+total += numeros[8]
+total += numeros[9]
+
+print('Total de la suma de los números de 1 a 10:', total)
+
+print()
+
+# Uso explícito del ciclo for para resolver un problema que requiere iterar (recorrer) todos los elementos 
+# de una colección (lista):
+
+total = 0
+
+for i in range(0, len(numeros)):
+    total += numeros[i]
+
+print('Total de la suma de los números de 1 a 10:', total)
