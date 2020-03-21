9039175796ac531cb6e9ab828aab804406759c6d
infzer0 <johnortizo@outlook.com>
2020-03-21 11:45:09 -0500

    Python - Curso V2 - Suma Elementos Lista con un Ciclo while
    Python Curso V2: 207 Realizar la Suma de los Elementos de una Lista con un Ciclo while.

--- parte09/9.2_ciclo_while.py
+++ parte09/9.2_ciclo_while.py
@@ -7,3 +7,21 @@ i = 0
 while i < len(numeros):
     print(numeros[i])
     i += 1
+
+print()
+
+print('Contenido de la lista `numeros`:', numeros)
+
+print()
+
+# Suma de los elementos de una lista con un ciclo while:
+print('Suma de los elementos de una lista con un ciclo while:')
+
+i = 0
+total = 0
+
+while i < len(numeros):
+    total += numeros[i]
+    i += 1
+
+print('Suma de los números de 1 a 5:', total)
