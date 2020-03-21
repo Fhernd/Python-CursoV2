50a481bc6bdde8f4cf7acc2fe923b17ea22cab35
infzer0 <johnortizo@outlook.com>
2020-03-21 11:56:53 -0500

    Python - Curso V2 - Suma Números Pares con un Ciclo while
    Python Curso V2: 207 Sumar los Números pares de una Lista con un Ciclo while.

--- parte09/9.2_ciclo_while.py
+++ parte09/9.2_ciclo_while.py
@@ -25,3 +25,27 @@ while i < len(numeros):
     i += 1
 
 print('Suma de los números de 1 a 5:', total)
+
+print()
+
+numeros.append(6)
+numeros.append(7)
+numeros.append(8)
+numeros.append(9)
+numeros.append(10)
+
+print('Contenido de la lista `numeros`:', numeros)
+
+print()
+
+# Suma de los valores pares en la lista `numeros`:
+
+suma_pares = 0
+i = 0
+
+while i < len(numeros):
+    if numeros[i] % 2 == 0:
+        suma_pares += numeros[i]
+    i += 1
+
+print('Suma de todos los valores pares que hay en el rango 1-10:', suma_pares)
