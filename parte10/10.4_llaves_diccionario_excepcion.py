19fd475c7f96885b52aef887f0b6a5a057abe4b2
infzer0 <johnortizo@outlook.com>
2020-03-24 13:34:46 -0500

    Python - Curso V2 - Gestión de Error KeyError
    Python Curso V2: 232 Gestión del Error KeyError con un Bloque try...except.

--- parte10/10.4_llaves_diccionario_excepcion.py
+++ parte10/10.4_llaves_diccionario_excepcion.py
@@ -4,6 +4,19 @@ versiones = {'Python': '3.8.1', 'Java': '12', 'JavaScript': 'ES6', 'C#': '8'}
 
 lenguaje = input('Escriba un nombre de lenguaje de programación: ')
 
-version = versiones[lenguaje]
+try:
+    version = versiones[lenguaje]
 
-print('La versión de {} es {}'.format(lenguaje, version))
+    print('La versión de {} es {}'.format(lenguaje, version))
+except KeyError as e:
+    print('ERROR:', e)
+
+print('Fin del programa.')
+
+print()
+
+# Uso del método `get()`:
+print('Uso del método `get()`:')
+
+version = versiones.get('java', '1.0.0')
+print('La versión de {} es {}'.format('java', version))
