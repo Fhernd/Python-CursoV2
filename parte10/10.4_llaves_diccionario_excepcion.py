64153821436cbf6de02b1b55c02a0be7cc466646
infzer0 <johnortizo@outlook.com>
2020-03-24 13:24:18 -0500

    Python - Curso V2 - KeyError
    Python Curso V2: 231 Llave no Disponible en un Diccionario - KeyError.

---
+++ parte10/10.4_llaves_diccionario_excepcion.py
@@ -0,0 +1,9 @@
+# Excepciones a la hora de trabajar con diccionarios.
+
+versiones = {'Python': '3.8.1', 'Java': '12', 'JavaScript': 'ES6', 'C#': '8'}
+
+lenguaje = input('Escriba un nombre de lenguaje de programación: ')
+
+version = versiones[lenguaje]
+
+print('La versión de {} es {}'.format(lenguaje, version))
