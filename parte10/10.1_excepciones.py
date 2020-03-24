b90f45e189124bc68beb1c7757ad7f943608adcd
infzer0 <johnortizo@outlook.com>
2020-03-24 11:45:23 -0500

    Python - Curso V2 - Bloque try...except
    Python Curso V2: 224 Tratamiento de la Generación del Error ValueError con el Bloque try...except.

--- parte10/10.1_excepciones.py
+++ parte10/10.1_excepciones.py
@@ -1,6 +1,12 @@
 # Excepciones en flujo de ejecución de un programa Python.
 
-numero = int(input('Escriba un número entero: '))
+try:
+    numero = int(input('Escriba un número entero: '))
 
-print('Contenido de la variable `numero`:', numero)
-print('El tipo de dato de la variable `numero` es:', type(numero))
+    print('Contenido de la variable `numero`:', numero)
+    print('El tipo de dato de la variable `numero` es:', type(numero))
+except ValueError as e:
+    print('ERROR: ', e)
+
+print()
+print('El programa ha terminado')
