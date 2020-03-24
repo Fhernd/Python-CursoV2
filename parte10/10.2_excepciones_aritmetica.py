6e0a0cc1947ac9e381369b9c1d23ffee985f6e87
infzer0 <johnortizo@outlook.com>
2020-03-24 12:18:00 -0500

    Python - Curso V2 - float()
    Python Curso V2: 227 Conversión de Datos de Punto Flotante Segura con Uso Excepciones.

---
+++ parte10/10.2_excepciones_aritmetica.py
@@ -0,0 +1,28 @@
+# Gestión de excepciones en operaciones aritméticas - división:
+print('Gestión de excepciones en operaciones aritméticas - división:')
+
+# Captura del primero número - dividendo:
+
+while True:
+    try:
+        dividendo = float(input('Escriba el dividendo: '))
+
+        break
+    except:
+        print('MENSAJE: Debe escribir un valor válido. Intente de nuevo.')
+    print()
+
+print()
+
+while True:
+    try:
+        divisor = float(input('Escriba el divisor: '))
+
+        break
+    except:
+        print('MENSAJE: Debe escribir un valor válido. Intente de nuevo.')
+    print()
+
+division = dividendo / divisor
+
+print('El resultado de la división es:', division)
