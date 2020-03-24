31a18f78c5436556c2ef68f12e634893d779f763
infzer0 <johnortizo@outlook.com>
2020-03-24 13:14:58 -0500

    Python - Curso V2 - Índice Inexistente - IndexError
    Python Curso V2: 230 Intento Fallido a Posiciones Negativas Inexistentes de una Lista - IndexError.

--- parte10/10.3_indices_lista.py
+++ parte10/10.3_indices_lista.py
@@ -15,4 +15,26 @@ try:
 except IndexError:
     print('El índice %i no existe en la lista `lenguajes`' % indice)
 
-print('El programa ha finalizado.')
+# print('El programa ha finalizado.')
+
+print()
+
+# Intento de acceso a índices negativos:
+print('Intento de acceso a índices negativos:')
+
+print('Último elemento de la lista `lenguajes`:', lenguajes[-1])
+
+print()
+
+indice = -7
+
+try:
+    lenguaje = lenguajes[indice]
+
+    print('Primer elemento de la lista `lenguajes`:', lenguaje)
+except:
+    print('El índice {} no existe en la lista `lenguajes`.'.format(indice))
+
+print()
+
+print('El programa Python ha terminado.')
