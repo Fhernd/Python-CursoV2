befd8f84c8f59ad777156a5d48edb51babf56bc4
infzer0 <johnortizo@outlook.com>
2020-03-24 11:34:04 -0500

    Python - Curso V2 - Excepciones
    Python Curso V2: 223 Primer Error al Intentar Convertir una Cadena a un Número - ValueError.

---
+++ parte10/10.1_excepciones.py
@@ -0,0 +1,6 @@
+# Excepciones en flujo de ejecución de un programa Python.
+
+numero = int(input('Escriba un número entero: '))
+
+print('Contenido de la variable `numero`:', numero)
+print('El tipo de dato de la variable `numero` es:', type(numero))
