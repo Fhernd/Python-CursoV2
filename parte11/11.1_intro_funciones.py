d6d1b76023b838f8a3b424a375a9b0297ea6c04f
infzer0 <johnortizo@outlook.com>
2020-03-28 17:03:11 -0500

    Python - Curso V2 - Documentar una Función
    Python Curso V2: Obtener Ayuda de una Función con help() y Documentar una Función.

--- parte11/11.1_intro_funciones.py
+++ parte11/11.1_intro_funciones.py
@@ -4,6 +4,16 @@
 print('1. Creación de una función:')
 
 def sumar(numero_1, numero_2):
+    """
+    Suma dos números (sean enteros o punto flotante).
+
+    Parameters:
+    numero_1: primer valor a sumar.
+    numero_2: segundo valor a sumar.
+
+    Returns:
+    Suma de dos números (enteros o reales).
+    """
     suma = numero_1 + numero_2
 
     return suma
@@ -19,3 +29,16 @@ print()
 # 2. Invocación de una función:
 resultado = sumar(2, 3)
 print('El resultado de sumar {} y {} es igual a {}.'.format(x, y, resultado))
+
+print()
+
+# 3. Obtener documentación/ayuda de una función:
+print('3. Obtener documentación/ayuda de una función:')
+
+print()
+
+help(sumar)
+
+print()
+
+help(print)
