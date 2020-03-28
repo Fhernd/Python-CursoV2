255509ed9de68ae40941fb65f40fb9871b152573
infzer0 <johnortizo@outlook.com>
2020-03-28 16:37:27 -0500

    Python - Curso V2 - Creación de una Función
    Python Curso V2: 239 Creación de una Función Personalizada - Uso de la Palabra Reservada def.

---
+++ parte11/11.1_intro_funciones.py
@@ -0,0 +1,9 @@
+# Introducción a las funciones - Unidades de reutilización y encapsulación de información:
+
+# 1. Creación de una función:
+print('1. Creación de una función:')
+
+def sumar(numero_1, numero_2):
+    suma = numero_1 + numero_2
+
+    return suma
