b226eb11fbfa5f8d8453282ecfc9fb542bbd8086
infzer0 <johnortizo@outlook.com>
2020-03-29 10:16:29 -0500

    Python - Curso V2 - Visualización de Invocación de Funciones
    Python Curso V2: 254 Visualización de la Ejecución de Fibonacci Iterativo y Fibonacci Recursivo.

--- parte11/11.6_funciones_recursivas.py
+++ parte11/11.6_funciones_recursivas.py
@@ -43,3 +43,50 @@ print('Factorial (iterativo):', resultado)
 resultado = factorial_recursivo(5)
 print('Factorial (recursivo):', resultado)
 
+print()
+
+# Función Fibonacci:
+print('Función Fibonacci:')
+
+# 0 1 1 2 3 5 8 13 21 34 55 89...
+
+def fibonacci_iterativo(n):
+    """
+    Calcula el n-ésimo valor de la serie Fibonacci. (Enfoque iterativo.)
+
+    Parameters:
+    n: n-ésimo valor de la serie Fibonacci a calcular.
+
+    Returns:
+    Valor de la serie Fibonacci.
+    """
+    a = 0
+    b = 1
+
+    for i in range(n - 1):
+        a, b = b, a + b
+    
+    return b
+
+def fibonacci_recursivo(n):
+    """
+    Calcula el n-ésimo valor de la serie Fibonacci. (Enfoque recursivo.)
+
+    Parameters:
+    n: n-ésimo valor de la serie Fibonacci a calcular.
+
+    Returns:
+    Valor de la serie Fibonacci.
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursivo(n - 2) + fibonacci_recursivo(n - 1)
+
+resultado = fibonacci_iterativo(10)
+print('Fibonacci (iterativo):', resultado)
+
+resultado = fibonacci_recursivo(10)
+print('Fibonacci (recursivo):', resultado)
