f6d1896d8e539ee65191668ea156ed0d620e42f4
infzer0 <johnortizo@outlook.com>
2020-03-29 07:31:11 -0500

    Python - Curso V2 - Recursividad
    Python Curso V2: 251 Definición de la Función Factorial - Enfoque Iterativo vs Recursivo.

---
+++ parte11/11.6_funciones_recursivas.py
@@ -0,0 +1,38 @@
+# Funciones recursivas:
+print('Funciones recursivas:')
+
+# n!
+# 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+def factorial_iterativo(n):
+    """
+    Calcula el factorial de un número. (Enfoque iterativo.)
+
+    Parameters:
+    n: Número de factorial a calcular.
+
+    Returns:
+    Factorial
+    """
+    resultado = 1
+
+    for i in range(1, n + 1):
+        resultado *= i
+    
+    return resultado
+
+def factorial_recursivo(n):
+    """
+    Calcula el factorial de un número. (Enfoque recursivo.)
+
+    Parameters:
+    n: Número de factorial a calcular.
+
+    Returns:
+    Factorial
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial_recursivo(n - 1)
+
