f63a55b91147813c2e4e4336057d86a364412ada
infzer0 <johnortizo@outlook.com>
2020-03-29 10:47:46 -0500

    Python - Curso V2 - Expresión Lambda para Calcular el Cuadrado de un Número
    Python Curso V2: 256 Expresión Lambda (Función Anónima) para Calcular el Cuadrado de un Número.

--- parte11/11.7_expresiones_lambda.py
+++ parte11/11.7_expresiones_lambda.py
@@ -14,3 +14,15 @@ print()
 
 sumar_lambda = lambda a, b: a + b
 print('La suma de {} + {} es igual a {}'.format(x, y, sumar_lambda(x, y)))
+
+print()
+
+def cuadrado(n):
+    return n ** 2
+
+cuadrado_lambda = lambda n: n ** 2
+
+numero = 10
+
+print('El cuadrado de {} es igual a {}'.format(numero, cuadrado(numero)))
+print('El cuadrado de {} es igual a {}'.format(numero, cuadrado_lambda(numero)))
