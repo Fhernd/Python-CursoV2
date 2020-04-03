66cea5a65b52eefdd3281118dd1fadbc803a59ca
infzer0 <johnortizo@outlook.com>
2020-04-03 15:57:53 -0500

    Python - Curso V2 - setup.py
    Python Curso V2: 277 Especificación de la Configuración del Módulo en el Archivo setup.py.

---
+++ parte12/proyecto/calculadora/__init__.py
@@ -0,0 +1,3 @@
+# Este archivo sirve para inicializar otros módulos.
+# Cada vez que se cree un módulo se define este archivo por convención.
+# De forma automática se realiza su invocación cada vez que se ejecute el proyecto.
