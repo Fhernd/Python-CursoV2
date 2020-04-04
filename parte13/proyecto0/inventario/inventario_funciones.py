5b19bd0af1c6c0780dbc77a6f885acb7cb00bcb2
infzer0 <johnortizo@outlook.com>
2020-04-04 18:20:17 -0500

    Python - Curso V2 - Codificación Funciones
    Python Curso V2: 283 Codificación de las Funciones buscar_producto() y cambiar_estado_producto().

--- parte13/proyecto0/inventario/inventario_funciones.py
+++ parte13/proyecto0/inventario/inventario_funciones.py
@@ -20,3 +20,29 @@ def realizar_venta(ventas, venta):
     """
     venta['fecha'] = datetime.now()
     ventas.append(venta)
+
+def buscar_producto(productos, id_producto):
+    """
+    Busca un producto a partir de su ID.
+
+    Parameters:
+    productos: lista de productos en el inventario
+    id_producto: ID del producto a buscar
+
+    Returns:
+    El producto encontrado, si no se encuentra None.
+    """
+    for p in productos:
+        if p['id_producto'] == id_producto:
+            return p
+    
+    return None
+
+def cambiar_estado_producto(producto):
+    """
+    Cambia el estado de un producto.
+
+    Parameters:
+    producto: Producto sobre el que se cambiará su estado.
+    """
+    producto['disponible'] = not producto['disponible']
