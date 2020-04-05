df53a9228bc1d5928e61d467ec573a3f4dfa80ca
infzer0 <johnortizo@outlook.com>
2020-04-05 09:39:16 -0500

    Python - Curso V2 - __main__.py
    Python Curso V2: 287 Inicio Códificación del Archivo __main__.py (Punto de Entrada Aplicación).

---
+++ parte13/proyecto0/inventario/__main__.py
@@ -0,0 +1,20 @@
+from .inventario_funciones import registrar_producto
+
+def mostrar_menu():
+    """
+    Muestra el menú de las operaciones disponibles.
+    """
+    print('1. Registrar nuevo producto')
+    print('2. Vender un producto')
+    print('3. Buscar un producto por su código')
+    print('4. Cambiar disponibilidad de un producto')
+    print('5. Productos vendidos en un rango de fecha')
+    print('6. Ver top 5 de los productos más vendidos')
+    print('7. Ver top 5 de los productos menos vendidos')
+    print('0. Salir')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
