c0e4113c3199a95873635b0506ac6eb441344f2a
infzer0 <johnortizo@outlook.com>
2020-04-08 12:23:12 -0500

    Python - Curso V2 - Crear una Carpeta
    Python Curso V2: Uso de la Función os.mkdir() para Crear una Carpeta en Disco.

--- parte14/14.8_operaciones_adicionales.py
+++ parte14/14.8_operaciones_adicionales.py
@@ -18,14 +18,44 @@ def main():
 
     print('Cambiar el nombre de un archivo:')
 
-    os.rename(nombre_archivo, 'parte14/archivo2.txt')
+    nuevo_nombre_archivo = 'parte14/archivo2.txt'
+
+    os.rename(nombre_archivo, nuevo_nombre_archivo)
 
     if os.path.isfile(nombre_archivo):
         print(f'El archivo {nombre_archivo} existe en disco.')
     else:
         print(f'El archivo {nombre_archivo} no existe en disco.')
 
+    print()
+
+    print('Eliminar un archivo')
+
+    os.remove(nuevo_nombre_archivo)
+
+    print()
+
+def crear_carpetas():
+    nombre_carpeta = 'parte14/código'
+
+    if os.path.isdir(nombre_carpeta):
+        print(f'La carpeta {nombre_carpeta} existe en disco.')
+    else:
+        print(f'La carpeta {nombre_carpeta} no existe en disco.')
+    
+    print()
+
+    print('Creación de una carpeta/directorio:')
+
+    os.mkdir(nombre_carpeta)
+
+    if os.path.isdir(nombre_carpeta):
+        print(f'La carpeta {nombre_carpeta} existe en disco.')
+    else:
+        print(f'La carpeta {nombre_carpeta} no existe en disco.')
+
 if __name__ == "__main__":
-    main()
+    # main()
+    crear_carpetas()
     
     print('Fin del programa.')
