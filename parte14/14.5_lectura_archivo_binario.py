d47ea8ee1f00530704e7d82ab5c4679144ff59a4
infzer0 <johnortizo@outlook.com>
2020-04-08 10:08:42 -0500

    Python - Curso V2 - Lectura Archivo Binario
    Python Curso V2: 313 Leer el Contenido de un Archivo Binario con la Función open().

---
+++ parte14/14.5_lectura_archivo_binario.py
@@ -0,0 +1,18 @@
+def main():
+    # Lectura del contenido de un archivo binario:
+    print('Lectura del contenido de un archivo binario:')
+
+    nombre_archivo = 'parte14/numeros.bin'
+
+    archivo = open(nombre_archivo, 'rb')
+
+    numeros = list(archivo.read())
+
+    print()
+
+    print(numeros)
+
+    print('El programa ha terminado.')
+
+if __name__ == "__main__":
+    main()
