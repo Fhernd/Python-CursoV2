202789f2bca2bba343d939de691257daf47cee76
infzer0 <johnortizo@outlook.com>
2020-05-05 07:18:26 -0500

    Python - Curso V2 - Inicio Creación Clase Carro
    Python Curso: 335 Crear un Archivo Python para Representar una Clase - Entidad Carro.

---
+++ parte16/modelos/carro.py
@@ -0,0 +1,7 @@
+class Carro:
+    def __init__(self, placa, marca, modelo, pais_procedencia):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.pais_procedencia = pais_procedencia
+        self.estado = False
