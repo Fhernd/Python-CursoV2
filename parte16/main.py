dd404948ea978d5d6998bee0740966b818e7e759
infzer0 <johnortizo@outlook.com>
2020-05-05 08:35:31 -0500

    Python - Curso V2 - Acceso a Métodos
    Python Curso V2: 339 Acceso a Miembros de Instancia con el Operador Punto - Métodos de Instancia.

--- parte16/main.py
+++ parte16/main.py
@@ -7,11 +7,36 @@ def main():
 
     print('El tipo de dato de la variable `carro_chevrolet` es:', type(carro_chevrolet).__name__)
 
+    print()
+
+    print('Contenido de las variables de instancia de la clase Carro:')
     print('Placa:', carro_chevrolet.placa)
     print('Marca:', carro_chevrolet.marca)
     print('Modelo:', carro_chevrolet.modelo)
     print('País procedencia:', carro_chevrolet.pais_procedencia)
     print('¿Está encendido?:', 'Sí' if carro_chevrolet.estado else 'No')
+
+    print()
+
+    print('Invocación de métodos de instancia de la clase Carro:')
+
+    carro_chevrolet.encender()
+
+    print('¿Está encendido?:', 'Sí' if carro_chevrolet.estado else 'No')
+
+    carro_chevrolet.apagar()
+
+    print('¿Está encendido?:', 'Sí' if carro_chevrolet.estado else 'No')
+
+    print()
+
+    carro_chevrolet.acelerar()
+    carro_chevrolet.frenar()
+
+    carro_chevrolet.encender()
+
+    carro_chevrolet.acelerar()
+    carro_chevrolet.frenar()
 
 if __name__ == '__main__':
     main()
