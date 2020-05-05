bc38e5b41afa8b1587754f5031012d805e40ca02
infzer0 <johnortizo@outlook.com>
2020-05-05 07:38:59 -0500

    Python - Curso V2 - Métodos de Instancia
    Python Curso: 336 Definición de Métodos de Instancia en una Clase - Entidad Carro.

--- parte16/modelos/carro.py
+++ parte16/modelos/carro.py
@@ -5,3 +5,19 @@ class Carro:
         self.modelo = modelo
         self.pais_procedencia = pais_procedencia
         self.estado = False
+    
+    def encender(self):
+        if not self.estado:
+            self.estado = True
+    
+    def apagar(self):
+        if self.estado:
+            self.estado = False
+    
+    def acelerar(self):
+        if self.estado:
+            print('El carro ha acelerado')
+    
+    def frenar(self):
+        if self.estado:
+            print('El carro ha frenado')
