29c7cd822ed631aaf6408f1a9fa75f1de044ac22
infzer0 <johnortizo@outlook.com>
2020-05-05 08:13:45 -0500

    Python - Curso V2 - Corrección Error ModuleNotFoundError
    Python Curso: 338 Ajustes en la Importación de una Clase desde un Módulo - ModuleNotFoundError.

--- parte16/main.py
+++ parte16/main.py
@@ -1,11 +1,17 @@
 # Creación de objetos (instanciación):
 
-from parte16.modelos import Carro
+from modelos.carro import Carro
 
 def main():
     carro_chevrolet = Carro('ABC-123', 'Chevrolet', 2010, 'Estados Unidos')
 
-    print('El tipo de dato de la variable `carro` es:', type(carro_chevrolet))
+    print('El tipo de dato de la variable `carro_chevrolet` es:', type(carro_chevrolet).__name__)
+
+    print('Placa:', carro_chevrolet.placa)
+    print('Marca:', carro_chevrolet.marca)
+    print('Modelo:', carro_chevrolet.modelo)
+    print('País procedencia:', carro_chevrolet.pais_procedencia)
+    print('¿Está encendido?:', 'Sí' if carro_chevrolet.estado else 'No')
 
 if __name__ == '__main__':
     main()
