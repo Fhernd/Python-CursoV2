a671bceccff65cb16877b1de73eb8c264fedd969
infzer0 <johnortizo@outlook.com>
2020-05-07 07:27:15 -0500

    Python - Curso V2 - Creación de la Clase Concreta Perro
    Python Curso V2: 359 Creación de la Clase Perro que Hereda de la Clase Abstracta Animal - Polimorfismo.

---
+++ parte16/animales/modelos/perro.py
@@ -0,0 +1,10 @@
+from . animal import Animal
+
+class Perro(Animal):
+    def __init__(self, nombre, edad, nombre_cientifico, raza):
+        super().__init__(nombre, edad, nombre_cientifico)
+
+        self.raza = raza
+    
+    def jugar(self):
+        print('El perro está jugando.')
