a196d5c870804463c19f1cee44c70d9a15d8f304
infzer0 <johnortizo@outlook.com>
2020-05-07 12:49:35 -0500

    Python - Curso V2 - Diagrama de Clases UML para la Jerarquía de Figuras
    Python Curso V2: 364 Codificación de la Clase Abstracta Figura - Polimorfismo.

---
+++ parte16/figuras/modelos/figura.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class Figura(ABC):
+    def __init__(self, color_fondo, color_borde):
+        self.color_fondo = color_fondo
+        self.color_borde = color_borde
+    
+    @abstractmethod
+    def dibujar(self):
+        pass
+    
+    @abstractmethod
+    def area(self):
+        pass
---
+++ parte16/figuras/modelos/rectangulo.py
@@ -0,0 +1,5 @@
+from .figura import Figura
+
+class Rectangulo(Figura):
+    def __init__(self, color_fondo, color_borde, ancho, alto):
+        
