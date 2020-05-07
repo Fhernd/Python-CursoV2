cb1243ec3added5e9c5f5e4f37a7131a7f04d8d6
infzer0 <johnortizo@outlook.com>
2020-05-07 12:59:42 -0500

    Python - Curso V2 - Clase Concreta Rectángulo
    Python Curso V2: 365 Codificación de la Clase Concreta Rectangulo - Polimorfismo.

--- parte16/figuras/modelos/rectangulo.py
+++ parte16/figuras/modelos/rectangulo.py
@@ -2,4 +2,15 @@ from .figura import Figura
 
 class Rectangulo(Figura):
     def __init__(self, color_fondo, color_borde, ancho, alto):
-        
+        super().__init__(color_fondo, color_borde)
+
+        self.ancho = ancho
+        self.alto = alto
+    
+    def dibujar(self):
+        print(f'Se está dibujando el rectángulo con medidas ancho {self.ancho} y alto {self.alto}.')
+    
+    def area(self):
+        resultado = self.ancho * self.alto
+
+        return resultado
