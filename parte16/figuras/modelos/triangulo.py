8969e49b3bd654aae4f701de5343a34de7b6f8d2
infzer0 <johnortizo@outlook.com>
2020-05-07 17:06:31 -0500

    Python - Curso V2 - Clase Concreta Triángulo
    Python Curso V2: 367 Codificación de la Clase Concreta Triángulo - Polimorfismo.

---
+++ parte16/figuras/modelos/triangulo.py
@@ -0,0 +1,14 @@
+from .figura import Figura
+
+class Triangulo(Figura):
+    def __init__(self, color_fondo, color_borde, base, altura):
+        super().__init__(color_fondo, color_borde)
+
+        self.base = base
+        self.altura = altura
+    
+    def dibujar(self):
+        print(f'Se está dibujando un triángulo con base {self.base} y altura {self.altura}.')
+    
+    def area(self):
+        resultado = self.base * self.altura / 2
