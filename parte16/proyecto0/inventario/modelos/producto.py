517a5fb794d0ced7ee417221b3744c5f0c607168
infzer0 <johnortizo@outlook.com>
2020-05-08 15:55:40 -0500

    Python - Curso V2 - Clases Producto y Venta
    Python Curso V2: 381 Implementación de las Clases Producto y Venta con Diagrama UML.

---
+++ parte16/proyecto0/inventario/modelos/producto.py
@@ -0,0 +1,8 @@
+class Producto:
+    def __init__(self, codigo, nombre, precio, cantidad):
+        self.codig = codigo
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+
+        self.disponible = True
---
+++ parte16/proyecto0/inventario/modelos/venta.py
@@ -0,0 +1,5 @@
+class Venta:
+    def __init__(self, codigo_producto, cantidad, total_sin_iva):
+        self.codigo_producto = codigo_producto
+        self.cantidad = cantidad
+        self.total_sin_iva = total_sin_iva
