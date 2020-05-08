117b9d1cf748989c422e3b6fb84b27091eb250f6
infzer0 <johnortizo@outlook.com>
2020-05-08 16:09:38 -0500

    Python - Curso V2 - Adaptación Funciones a Métodos de Instancia
    Python Curso V2: 382 Adaptación de las Funciones para ser Tratadas como Métodos de Instancia.

--- parte16/proyecto0/inventario/modelos/producto.py
+++ parte16/proyecto0/inventario/modelos/producto.py
@@ -1,6 +1,6 @@
 class Producto:
     def __init__(self, codigo, nombre, precio, cantidad):
-        self.codig = codigo
+        self.codigo = codigo
         self.nombre = nombre
         self.precio = precio
         self.cantidad = cantidad
