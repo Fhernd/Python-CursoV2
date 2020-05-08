bac5b1d4c965c863243d1db2e20eb8f30fc10bc6
infzer0 <johnortizo@outlook.com>
2020-05-08 10:36:32 -0500

    Python - Curso V2 - Subclase EmpleadoNomina
    Python Curso V2: 377 Sobreescribiendo el Método calcular_salario() en la Subclase EmpleadoNomina.

---
+++ parte16/empleados/modelos/empleado_nomina.py
@@ -0,0 +1,14 @@
+from .empleado import Empleado
+
+class EmpleadoNomina(Empleado):
+    SALARIO = 2000
+
+    def __init__(self, documento, nombre_completo, correo_electronico, especialidad, porcentaje_prestaciones):
+        super().__init__(documento, nombre_completo, correo_electronico, especialidad)
+
+        self.porcentaje_prestaciones = porcentaje_prestaciones
+    
+    def calcular_salario(self):
+        total = super().calcular_salario() + SALARIO * (1 - self.porcentaje_prestaciones)
+
+        return total
