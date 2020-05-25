12552b009ff35790ffb4d03f4429ae4b2462c7b7
infzer0 <johnortizo@outlook.com>
2020-05-25 08:11:22 -0500

    Python Curso V2: QLabel Componente de GUI
    Python Curso V2: 393 Mostrar Texto Personalizado en un Componente Tipo QLabel.

--- parte17/demo02_qlabel.py
+++ parte17/demo02_qlabel.py
@@ -1,19 +1,23 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        self.setWindowTitle('¡Hola, PyQt5!')
+        self.initUi()
+    
+    def initUi(self):
+        self.setWindowTitle('Demostración del Componente QLabel')
 
         self.setFixedSize(500, 400)
 
+        self.lbl_mensaje = QLabel('¡PyQt5 es tremendo!', self)
+        self.lbl_mensaje.move(100, 50)
 
 def main():
     app = QApplication(sys.argv)
-
     ventana = VentanaPrincipal()
     ventana.show()
 
