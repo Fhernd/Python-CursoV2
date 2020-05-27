2dea220ccb749f297657bdcbc8ff7cf4d68bfa8a
infzer0 <johnortizo@outlook.com>
2020-05-27 14:40:47 -0500

    Python Curso V2: Componentes QLabel, QLineEdit, QPushButton
    Python Curso V2: 400 2/4 Calculadora Básica con Interfaz Gráfica de Usuario (GUI) con PyQt5.

--- parte17/demo07_calculadora_suma.py
+++ parte17/demo07_calculadora_suma.py
@@ -10,8 +10,34 @@ class CalculadoraVentana(QMainWindow):
     
     def initUi(self):
         self.setWindowTitle('Calculadora Básica')
-        self.setFixedSize(400, 350)
+        self.setFixedSize(400, 300)
 
+        self.lbl_primer_numero = QLabel('Primer número:', self)
+        self.lbl_primer_numero.move(30, 30)
+
+        self.txt_primer_numero = QLineEdit(self)
+        self.txt_primer_numero.move(150, 30)
+        self.txt_primer_numero.setFixedWidth(200)
+
+        self.lbl_segundo_numero = QLabel('Segundo número:', self)
+        self.lbl_segundo_numero.move(30, 80)
+
+        self.txt_segundo_numero = QLineEdit(self)
+        self.txt_segundo_numero.move(150, 80)
+        self.txt_segundo_numero.setFixedWidth(200)
+
+        self.btn_calcular_suma = QPushButton('Sumar', self)
+        self.btn_calcular_suma.move(150, 130)
+        self.btn_calcular_suma.setFixedWidth(200)
+
+        self.lbl_resultado = QLabel('Resultado:', self)
+        self.lbl_resultado.move(30, 200)
+
+        self.txt_resultado = QLineEdit(self)
+        self.txt_resultado.move(150, 200)
+        self.txt_resultado.setFixedWidth(200)
+
+        
 
 def main():
     app = QApplication(sys.argv)
