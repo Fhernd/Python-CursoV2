3dad15f5b97b54ca355b54136b1a547921c74742
infzer0 <johnortizo@outlook.com>
2020-05-30 17:08:33 -0500

    Python Curso V2: 410 3/3 Crear una Barra de Menú para una Ventana Principal (QMainWindow)
    Crear una Barra de Menú para una Ventana Principal (QMainWindow) con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo11_aplicacion_con_barra_menu.py
+++ parte17/demo11_aplicacion_con_barra_menu.py
@@ -12,6 +12,42 @@ class AplicacionVentana(QMainWindow):
         self.setWindowTitle('Aplicación Principal')
         self.setFixedSize(500, 400)
 
+        self.mbr_principal = self.menuBar()
+        self.mnu_archivo = self.mbr_principal.addMenu('Archivo')
+        self.mnu_operaciones = self.mbr_principal.addMenu('Operaciones')
+        self.mnu_ayuda = self.mbr_principal.addMenu('Ayuda')
+
+        self.mni_salir = QAction('Salir', self)
+        self.mni_salir.setShortcut('Ctrl+Q')
+        self.mni_salir.setStatusTip('Finaliza la aplicación')
+        self.mni_salir.triggered.connect(self.close)
+
+        self.mni_copiar = QAction('Copiar', self)
+        self.mni_copiar.setShortcut('Ctrl+C')
+        self.mnu_operaciones.addAction(self.mni_copiar)
+        
+        self.mni_cortar = QAction('Cortar', self)
+        self.mni_cortar.setShortcut('Ctrl+X')
+        self.mnu_operaciones.addAction(self.mni_cortar)
+
+        self.mni_pegar = QAction('Pegar', self)
+        self.mni_pegar.setShortcut('Ctrl+V')
+        self.mnu_operaciones.addAction(self.mni_pegar)
+        
+        self.mnu_archivo.addAction(self.mni_salir)
+
+        self.mni_acerca_de = QAction('Acerca de')
+        self.mnu_ayuda.addAction(self.mni_acerca_de)
+        self.mni_acerca_de.triggered.connect(self.mostrar_acerca_de)
+
+    def mostrar_acerca_de(self):
+        mensaje = QMessageBox()
+        mensaje.setWindowTitle('Acerca de...')
+        mensaje.setIcon(QMessageBox.Information)
+        mensaje.setText('Desarrollador: John Ortiz Ordoñez - Versión: 1.0.0')
+
+        mensaje.exec_()
+
 
 def main():
     app = QApplication(sys.argv)
