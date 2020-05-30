4c2b332110d2e3b02ff13ce264b7d2fb7c9b9555
infzer0 <johnortizo@outlook.com>
2020-05-30 12:10:58 -0500

    Python Curso V2: 406 2/3 Diálogo de Confirmación para Eliminar un Producto usando QMessageBox
    Diálogo de Confirmación para Eliminar un Producto usando QMessageBox con el lenguaje de programación Python y la librería PyQt.

--- parte17/demo10_dialog_confirmacion_eliminacion.py
+++ parte17/demo10_dialog_confirmacion_eliminacion.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMessageBox, QPushButton
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QMainWindow, QMessageBox, QPushButton
+from PyQt5.QtGui import QIntValidator
 
 class EliminacionProductoVentana(QMainWindow):
     def __init__(self):
@@ -10,7 +11,26 @@ class EliminacionProductoVentana(QMainWindow):
     
     def initGui(self):
         self.setWindowTitle('Eliminación Producto Por ID')
-        self.setFixedSize(400, 350)
+        self.setFixedSize(400, 200)
+
+        self.lbl_producto_id = QLabel('Producto ID:', self)
+        self.lbl_producto_id.move(30, 30)
+
+        self.txt_producto_id = QLineEdit(self)
+        self.txt_producto_id.move(150, 30)
+        self.txt_producto_id.setValidator(QIntValidator())
+        self.txt_producto_id.setFixedWidth(200)
+
+        self.btn_eliminar_producto = QPushButton('Eliminar', self)
+        self.btn_eliminar_producto.move(150, 70)
+        self.btn_eliminar_producto.setFixedWidth(200)
+
+        self.lbl_resultado = QLabel('Resultado:', self)
+        self.lbl_resultado.move(30, 120)
+
+        self.txt_resultado = QLineEdit(self)
+        self.txt_resultado.move(150, 120)
+        self.txt_resultado.setFixedWidth(200)
 
 
 def main():
