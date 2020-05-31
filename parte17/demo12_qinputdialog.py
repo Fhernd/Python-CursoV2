f242ee4b34cc7ad916f1972e421599e4e0b565ad
infzer0 <johnortizo@outlook.com>
2020-05-31 13:36:55 -0500

    Python Curso V2: 412 2/5 Captura de Datos Personales con Diálogos Emergentes - QInputDialog
    Captura de Datos Personales con Diálogos Emergentes - QInputDialog con el lenguaje de programación Python.

--- parte17/demo12_qinputdialog.py
+++ parte17/demo12_qinputdialog.py
@@ -12,6 +12,33 @@ class CapturaDatosVentana(QMainWindow):
         self.setWindowTitle('Captura de Datos - QInputDialog')
         self.setFixedSize(400, 500)
 
+        self.btn_nombre = QPushButton('Capturar nombre', self)
+        self.btn_nombre.move(100, 30)
+        self.btn_nombre.setFixedWidth(200)
+
+        self.btn_edad = QPushButton('Capturar edad', self)
+        self.btn_edad.move(100, 70)
+        self.btn_edad.setFixedWidth(200)
+
+        self.btn_ahorros = QPushButton('Capturar ahorros', self)
+        self.btn_ahorros.move(100, 110)
+        self.btn_ahorros.setFixedWidth(200)
+
+        self.btn_color = QPushButton('Capturar color', self)
+        self.btn_color.move(100, 150)
+        self.btn_color.setFixedWidth(200)
+
+        lbl_resumen = QLabel('Resumen:', self)
+        lbl_resumen.move(180, 220)
+
+        lbl_nombre = QLabel('Nombre:', self)
+        lbl_nombre.move(30, 270)
+
+        self.txt_nombre = QLineEdit(self)
+        self.txt_nombre.move(170, 270)
+        self.txt_nombre.setFixedWidth(200)
+        self.txt_nombre.setEnabled(False)
+
         
 
 
