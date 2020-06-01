3fc13fa9055122a86cb4b8cd21cb16711d9ea30e
infzer0 <johnortizo@outlook.com>
2020-06-01 07:27:38 -0500

    Python Curso V2: 417 2/2 Selección Exclusiva de una Clase de Vuelo con Botones Tipo Radio - QRadioButton
    Selección Exclusiva de una Clase de Vuelo con Botones Tipo Radio - QRadioButton con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo13_qradiobutton.py
+++ parte17/demo13_qradiobutton.py
@@ -12,6 +12,39 @@ class SeleccionClaseVueloVentana(QMainWindow):
         self.setWindowTitle('Selección Clase Vuelo')
         self.setFixedSize(350, 300)
 
+        lbl_seleccion_clase_vuelo = QLabel('Seleccione la clase de su vuelo:', self)
+        lbl_seleccion_clase_vuelo.move(30, 30)
+        lbl_seleccion_clase_vuelo.setFixedWidth(200)
+
+        self.rbn_clase_primera = QRadioButton('Primera clase', self)
+        self.rbn_clase_primera.move(30, 80)
+        self.rbn_clase_primera.toggled.connect(self.seleccionar_clase)
+
+        self.rbn_clase_negocios = QRadioButton('Clase negocios', self)
+        self.rbn_clase_negocios.move(30, 110)
+        self.rbn_clase_negocios.toggled.connect(self.seleccionar_clase)
+
+        self.rbn_clase_economica = QRadioButton('Clase económica', self)
+        self.rbn_clase_economica.move(30, 140)
+        self.rbn_clase_economica.toggled.connect(self.seleccionar_clase)
+
+        self.lbl_resultado_seleccion = QLabel('', self)
+        self.lbl_resultado_seleccion.move(30, 180)
+        self.lbl_resultado_seleccion.setFixedWidth(200)
+
+    def seleccionar_clase(self):
+
+        costo_vuelo = 0
+
+        if self.rbn_clase_primera.isChecked():
+            costo_vuelo = 2000
+        if self.rbn_clase_negocios.isChecked():
+            costo_vuelo = 1500
+        if self.rbn_clase_economica.isChecked():
+            costo_vuelo = 1000
+        
+        self.lbl_resultado_seleccion.setText(f'Costo del tiquete: {costo_vuelo}')
+
 
 def main():
     app = QApplication(sys.argv)
