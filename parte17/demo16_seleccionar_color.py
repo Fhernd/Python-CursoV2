72ce5cab5e043497d287d41bd02ea7f1f36e3dae
infzer0 <johnortizo@outlook.com>
2020-06-01 12:50:03 -0500

    Python Curso V2: 423 2/2 Diálogo para la Selección de un Color - QColorDialog
    Diálogo para la Selección de un Color - QColorDialog con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo16_seleccionar_color.py
+++ parte17/demo16_seleccionar_color.py
@@ -11,8 +11,23 @@ class SeleccionColorVentana(QMainWindow):
 
     def inicializarGui(self):
         self.setWindowTitle('Selección Color')
-        self.setFixedSize(400, 300)
+        self.setFixedSize(400, 200)
 
+        self.btn_seleccionar_color = QPushButton('Seleccionar color...', self)
+        self.btn_seleccionar_color.move(100, 30)
+        self.btn_seleccionar_color.setFixedWidth(200)
+        self.btn_seleccionar_color.clicked.connect(self.seleccionar_color)
+
+        self.lbl_resultado_seleccion = QLabel('', self)
+        self.lbl_resultado_seleccion.move(190, 100)
+        self.lbl_resultado_seleccion.setFixedWidth(100)
+    
+    def seleccionar_color(self):
+        color = QColorDialog.getColor()
+
+        if color.isValid():
+            self.lbl_resultado_seleccion.setText(color.name())
+            self.lbl_resultado_seleccion.setStyleSheet(f'color: {color.name()}')
 
 def main():
     app = QApplication(sys.argv)
