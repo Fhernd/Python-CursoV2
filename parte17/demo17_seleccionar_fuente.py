d784441323547a897c40b3a7df856f4a14cc47d6
infzer0 <johnortizo@outlook.com>
2020-06-01 13:26:52 -0500

    Python Curso V2: 425 2/2 Diálogo para la Selección de un Fuente de Texto - QFontDialog
    Diálogo para la Selección de un Fuente de Texto - QFontDialog con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo17_seleccionar_fuente.py
+++ parte17/demo17_seleccionar_fuente.py
@@ -12,6 +12,22 @@ class SeleccionFuenteVentana(QMainWindow):
         self.setWindowTitle('Selección Fuente')
         self.setFixedSize(400, 250)
 
+        self.btn_seleccionar_fuente = QPushButton('Seleccionar fuente...', self)
+        self.btn_seleccionar_fuente.move(100, 30)
+        self.btn_seleccionar_fuente.setFixedWidth(200)
+        self.btn_seleccionar_fuente.clicked.connect(self.seleccionar_fuente)
+
+        self.lbl_resultado_seleccion = QLabel('', self)
+        self.lbl_resultado_seleccion.move(30, 150)
+        self.lbl_resultado_seleccion.setFixedWidth(250)
+
+    def seleccionar_fuente(self):
+        fuente, ok = QFontDialog.getFont()
+
+        if ok:
+            self.lbl_resultado_seleccion.setText(fuente.toString())
+            self.lbl_resultado_seleccion.setFont(fuente)
+
 def main():
     app = QApplication(sys.argv)
     ventana = SeleccionFuenteVentana()
