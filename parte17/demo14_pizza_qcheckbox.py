5d8da04cd394ff7b4101aa51a24780d97af2500e
infzer0 <johnortizo@outlook.com>
2020-06-01 08:04:28 -0500

    Python Curso V2: 419 2/3 Selección de Ingredientes Adicionales para una Pizza - QCheckBox
    Selección de Ingredientes Adicionales para una Pizza - QCheckBox con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo14_pizza_qcheckbox.py
+++ parte17/demo14_pizza_qcheckbox.py
@@ -12,6 +12,26 @@ class PizzaVentana(QMainWindow):
         self.setWindowTitle('Pizza & Ingredientes')
         self.setFixedSize(400, 350)
 
+        lbl_pizza = QLabel('Precio base pizza: $15', self)
+        lbl_pizza.move(30, 30)
+
+        lbl_ingredientes_adicionales = QLabel('Ingredientes adicionales:', self)
+        lbl_ingredientes_adicionales.move(30, 80)
+
+        self.chk_queso = QCheckBox('Queso ($1)', self)
+        self.chk_queso.move(30, 110)
+
+        self.chk_aceitunas = QCheckBox('Aceitunas ($2)', self)
+        self.chk_aceitunas.move(30, 140)
+
+        self.chk_salsas = QCheckBox('Salsas ($1)', self)
+        self.chk_salsas.move(30, 170)
+
+        self.lbl_resultado = QLabel('', self)
+        self.lbl_resultado.move(30, 220)
+
+        
+
 
 def main():
     app = QApplication(sys.argv)
