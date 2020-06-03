571f9ab8938b0bf7ad45599191831069433d2046
infzer0 <johnortizo@outlook.com>
2020-06-03 16:12:27 -0500

    Python Curso V2: 434 2/2 Crear una Barra de Herramientas para Operaciones Básicas de Archivos
    Crear una Barra de Herramientas para Operaciones Básicas de Archivos con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo23_barra_herramientas.py
+++ parte17/demo23_barra_herramientas.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QAction, QApplication, QMainWindow, QPlainTextEdit
+from PyQt5.QtWidgets import QAction, QApplication, QFileDialog, QMainWindow, QPlainTextEdit
 from PyQt5.QtGui import QIcon
 
 class BarraHerramientasVentana(QMainWindow):
@@ -15,19 +15,39 @@ class BarraHerramientasVentana(QMainWindow):
 
         barra_herramientas = self.addToolBar('Archivo')
 
-        nuevo_archivo = QAction(QIcon('parte17/new.png'), 'Nuevo', self)
-        barra_herramientas.addAction(nuevo_archivo)
+        mni_nuevo_archivo = QAction(QIcon('parte17/new.png'), 'Nuevo', self)
+        mni_nuevo_archivo.triggered.connect(self.crear_archivo)
+        barra_herramientas.addAction(mni_nuevo_archivo)
 
-        abrir_archivo = QAction(QIcon('parte17/open.png'), 'Abrir', self)
-        barra_herramientas.addAction(abrir_archivo)
+        mni_abrir_archivo = QAction(QIcon('parte17/open.png'), 'Abrir', self)
+        mni_abrir_archivo.triggered.connect(self.abrir_archivo)
+        barra_herramientas.addAction(mni_abrir_archivo)
 
-        guardar_archivo = QAction(QIcon('parte17/save.png'), 'Guardar', self)
-        barra_herramientas.addAction(guardar_archivo)
+        mni_guardar_archivo = QAction(QIcon('parte17/save.png'), 'Guardar', self)
+        mni_guardar_archivo.triggered.connect(self.guardar_archivo)
+        barra_herramientas.addAction(mni_guardar_archivo)
 
         self.contenido = QPlainTextEdit(self)
         self.contenido.move(10, 50)
         self.contenido.setFixedWidth(380)
         self.contenido.setFixedHeight(240)
+    
+    def crear_archivo(self):
+        self.contenido.clear()
+
+    def abrir_archivo(self):
+        archivo, ok = QFileDialog.getOpenFileName(self, 'Seleccionar archivo de texto...', 'C:\\', 'Archivo de texto (*.txt)')
+
+        if ok:
+            with open(archivo, 'r') as f:
+                self.contenido.insertPlainText(''.join(f.readlines()))
+
+    def guardar_archivo(self):
+        archivo, ok = QFileDialog.getSaveFileName(self, 'Guardar archivo de texto...', 'C:\\', 'Archivo de texto (*.txt)')
+
+        if ok:
+            with open(archivo, 'w') as f:
+                f.write(self.contenido.toPlainText())
 
 def main():
     app = QApplication(sys.argv)
