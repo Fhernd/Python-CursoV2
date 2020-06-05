20db49c746c0c0dc5286cb867b21be82e73cca3b
infzer0 <johnortizo@outlook.com>
2020-06-05 07:47:20 -0500

    Python Curso V2: 441 2/2 Organización de una Interfaz Gráfica por medio de Pestañas (QTabWidget)
    Organización de una Interfaz Gráfica por medio de Pestañas (QTabWidget) con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo28_qtabwidget.py
+++ parte17/demo28_qtabwidget.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QLineEdit, QTabWidget, QWidget
+from PyQt5.QtWidgets import QApplication, QComboBox, QFormLayout, QHBoxLayout, QLineEdit, QPushButton, QRadioButton, QTabWidget, QWidget
 
 class CapturaDatos(QTabWidget):
     def __init__(self):
@@ -25,13 +25,40 @@ class CapturaDatos(QTabWidget):
         self.inicializar_tab_datos_educacion()
     
     def inicializar_tab_datos_basicos(self):
-        pass
+        layout = QFormLayout()
+        layout.addRow('Nombres:', QLineEdit())
+        layout.addRow('Apellidos:', QLineEdit())
+
+        lay_genero = QHBoxLayout()
+        lay_genero.addWidget(QRadioButton('Hombre'))
+        lay_genero.addWidget(QRadioButton('Mujer'))
+        layout.addRow('Género:', lay_genero)
+
+        self.tab_datos_basicos.setLayout(layout)
+
 
     def inicializar_tab_datos_contacto(self):
-        pass
+        layout = QFormLayout()
+        layout.addRow('Correo-e:', QLineEdit())
+        layout.addRow('Dirección:', QLineEdit())
+
+        self.tab_datos_contacto.setLayout(layout)
     
     def inicializar_tab_datos_educacion(self):
-        pass
+        layout = QFormLayout()
+        
+        cbx_nivel_educativo = QComboBox()
+        cbx_nivel_educativo.addItem('Primaria')
+        cbx_nivel_educativo.addItem('Secundaria')
+        cbx_nivel_educativo.addItem('Profesional')
+        cbx_nivel_educativo.addItem('Maestría')
+        cbx_nivel_educativo.addItem('Doctorado')
+
+        layout.addRow('Nivel educativo:', cbx_nivel_educativo)
+        layout.addRow('Profesión:', QLineEdit())
+        layout.addRow('', QPushButton('Guardar datos'))
+
+        self.tab_datos_educacion.setLayout(layout)
 
 def main():
     app = QApplication(sys.argv)
