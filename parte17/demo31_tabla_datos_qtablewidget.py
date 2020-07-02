3991616848f2af368d9e4562b60727a3ca02f6f8
infzer0 <johnortizo@outlook.com>
2020-07-02 17:06:57 -0500

    Python Curso V2: 447 2/2 Crear una Tabla de Datos a través del Componente QTableWidget
    Crear una Tabla de Datos a través del Componente QTableWidget con el lenguaje de programación Python.

--- parte17/demo31_tabla_datos_qtablewidget.py
+++ parte17/demo31_tabla_datos_qtablewidget.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
 
 class TablaDatosVentana(QMainWindow):
     def __init__(self):
@@ -24,9 +24,21 @@ class TablaDatosVentana(QMainWindow):
         self.tbl_datos.setItem(1, 0, QTableWidgetItem('Dato 4'))
         self.tbl_datos.setItem(1, 1, QTableWidgetItem('Dato 5'))
         self.tbl_datos.setItem(1, 2, QTableWidgetItem('Dato 6'))
-        self.tbl_datos.setItem(1, 0, QTableWidgetItem('Dato 4'))
-        self.tbl_datos.setItem(1, 1, QTableWidgetItem('Dato 5'))
-        self.tbl_datos.setItem(1, 2, QTableWidgetItem('Dato 6'))
+        self.tbl_datos.setItem(2, 0, QTableWidgetItem('Dato 7'))
+        self.tbl_datos.setItem(2, 1, QTableWidgetItem('Dato 8'))
+        self.tbl_datos.setItem(2, 2, QTableWidgetItem('Dato 9'))
+
+        self.tbl_datos.doubleClicked.connect(self.seleccionar_elemento)
+
+        widget = QWidget(self)
+        self.setCentralWidget(widget)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tbl_datos)
+        widget.setLayout(self.layout)
+
+    def seleccionar_elemento(self):
+        print('Se ha hecho doble click sobre la tabla.')
 
 def main():
     app = QApplication(sys.argv)
