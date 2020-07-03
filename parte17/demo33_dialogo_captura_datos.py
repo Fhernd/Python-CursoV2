08aed7eafba99bd890900ea3ed3b92b826b14893
infzer0 <johnortizo@outlook.com>
2020-07-03 13:21:51 -0500

    Python Curso V2: 551 2/2 Ventana Tipo Diálogo (QDialog) Formulario de Registro de Datos
    Ventana Tipo Diálogo (QDialog) Formulario de Registro de Datos con el lenguaje de programación Python.

--- parte17/demo33_dialogo_captura_datos.py
+++ parte17/demo33_dialogo_captura_datos.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QComboBox, QDialog, QDialogButtonBox, QFormLayout, QGroupBox, QLabel, QLineEdit, QSpinBox
+from PyQt5.QtWidgets import QApplication, QComboBox, QDialog, QDialogButtonBox, QFormLayout, QGroupBox, QLabel, QLineEdit, QMessageBox, QSpinBox, QVBoxLayout
 
 class RegistroDialogo(QDialog):
     def __init__(self):
@@ -10,16 +10,50 @@ class RegistroDialogo(QDialog):
 
     def inicializarGui(self):
         self.setWindowTitle('Registro Datos')
-        self.setFixedSize(400, 400)
+        self.setFixedSize(400, 150)
 
         self.crearFormularioRegistro()
+
+        botones = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        botones.accepted.connect(self.registrar)
+        botones.rejected.connect(self.cancelar)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.gbx_controles)
+        layout.addWidget(botones)
+
+        self.setLayout(layout)
+    
+    def registrar(self):
+        mensaje = QMessageBox(self)
+        mensaje.setIcon(QMessageBox.Information)
+        mensaje.setWindowTitle('Mensaje')
+        mensaje.setText('Los datos se registraron de forma satisfactoria.')
+        mensaje.exec_()
+
+    def cancelar(self):
+        mensaje = QMessageBox(self)
+        mensaje.setIcon(QMessageBox.Warning)
+        mensaje.setWindowTitle('Advertencia')
+        mensaje.setText('Los datos no se han registrado.')
+        mensaje.exec_()
     
     def crearFormularioRegistro(self):
         self.gbx_controles = QGroupBox('Datos Básicos')
 
         layout = QFormLayout()
         layout.addRow(QLabel('Nombre:'), QLineEdit())
-        layout.addRow(QLabel('País:'), QComboBox())
+        
+        paises = QComboBox()
+        paises.addItem('Colombia')
+        paises.addItem('Estados Unidos')
+        paises.addItem('Alemania')
+        paises.addItem('Perú')
+        paises.addItem('Argentina')
+        paises.addItem('China')
+        paises.addItem('Rusia')
+        layout.addRow(QLabel('País:'), paises)
+
         layout.addRow(QLabel('Edad:'), QSpinBox())
 
         self.gbx_controles.setLayout(layout)
