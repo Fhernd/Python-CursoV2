3dd1c0273bc70d5c35cbcea6b17ec0acf034068d
infzer0 <johnortizo@outlook.com>
2020-07-18 13:38:28 -0500

    Python Curso V2: 525 3/3 Aplicación Básica de Editor Gráfico - Interfaz Creada en Qt Designer
    Aplicación Básica de Editor Gráfico - Interfaz Creada en Qt Designer con el lenguaje de programación Python.

--- parte17/demo76_aplicacion_editor_grafico.py
+++ parte17/demo76_aplicacion_editor_grafico.py
@@ -1,6 +1,8 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtGui import QPainter
 from demo76_editor_grafico import Ui_EditorGrafico
 
 class AplicacionEditorGrafico(QMainWindow):
@@ -18,21 +20,61 @@ class AplicacionEditorGrafico(QMainWindow):
         self.ui.mni_rectangulo.triggered.connect(self.dibujar_rectangulo)
         self.ui.mni_linea.triggered.connect(self.dibujar_linea)
 
-        self.ui.mni_acerca_de.triggered.connect(self.mostrar_acerca_def)
+        self.ui.mni_acerca_de.triggered.connect(self.mostrar_acerca_de)
+
+        self.posicion_1 = [0, 0]
+        self.posicion_2 = [0, 0]
+        self.tipo_dibujo = ''
 
         self.show()
     
     def dibujar_circulo(self):
-        pass
+        self.tipo_dibujo = 'círculo'
 
     def dibujar_rectangulo(self):
-        pass
+        self.tipo_dibujo = 'rectángulo'
 
     def dibujar_linea(self):
-        pass
+        self.tipo_dibujo = 'línea'
     
-    def mostrar_acerca_def(self):
-        pass
+    def mostrar_acerca_de(self):
+        mensaje = QMessageBox(self)
+        mensaje.setWindowTitle('Acerca de...')
+        mensaje.setIcon(QMessageBox.Information)
+        mensaje.setText('Versión: 1.0.0\nDesarrollador: John Ortiz Ordoñez')
+        mensaje.exec_()
+    
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            self.posicion_1[0], self.posicion_1[1] = event.pos().x(), event.pos().y()
+    
+    def mouseReleaseEvent(self, event):
+        self.posicion_2[0], self.posicion_2[1] = event.pos().x(), event.pos().y()
+        self.update()
+    
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+
+        if self.tipo_dibujo == 'círculo':
+            ancho = self.posicion_1[0] - self.posicion_2[0]
+            alto = self.posicion_1[1] - self.posicion_2[1]
+
+            rectangulo = QRect(self.posicion_1[0], self.posicion_1[1], ancho, alto)
+            angulo_inicio = 0
+            longitud_arco = 360 * 16
+            painter.drawArc(rectangulo, angulo_inicio, longitud_arco)
+        
+        if self.tipo_dibujo == 'rectángulo':
+            ancho = self.posicion_2[0] - self.posicion_1[0]
+            alto = self.posicion_2[1] - self.posicion_1[1]
+
+            painter.drawRect(self.posicion_1[0], self.posicion_1[1], ancho, alto)
+        
+        if self.tipo_dibujo == 'línea':
+            painter.drawLine(self.posicion_1[0], self.posicion_1[1], self.posicion_2[0], self.posicion_2[1])
+        
+        painter.end()
 
 def main():
     app = QApplication(sys.argv)
