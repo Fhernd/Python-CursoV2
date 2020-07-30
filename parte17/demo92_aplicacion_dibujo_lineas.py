ef30c71d0aa4c2d08b97baa6d3d76327cd2623a5
infzer0 <johnortizo@outlook.com>
2020-07-30 18:18:02 -0500

    Python Curso V2: 562 2/2 Dibujar Diferentes Tipos de Líneas en una Interfaz Gráfica usando el Mouse
    Dibujar Diferentes Tipos de Líneas en una Interfaz Gráfica usando el Mouse con el lenguaje de programación de Python y la librería de PyQt5.

--- parte17/demo92_aplicacion_dibujo_lineas.py
+++ parte17/demo92_aplicacion_dibujo_lineas.py
@@ -5,3 +5,56 @@ from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt
 from demo92_dibujo_lineas import Ui_DibujoLineas
 
+class AplicacionDibujoLineas(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        self.ui = Ui_DibujoLineas()
+        self.ui.setupUi(self)
+
+        self.posicion_1 = [0, 0]
+        self.posicion_2 = [0, 0]
+        self.tipo_linea = None
+
+        self.tipos_lineas = {0: Qt.SolidLine, 1: Qt.DashLine, 2: Qt.DashDotLine, 3: Qt.DotLine, 4: Qt.DashDotDotLine}
+
+        self.show()
+    
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            self.posicion_1[0] = event.pos().x()
+            self.posicion_1[1] = event.pos().y()
+    
+    def mouseReleaseEvent(self, event):
+        self.tipo_linea = self.ui.lst_tipos_lineas.currentRow()
+
+        self.posicion_2[0] = event.pos().x()
+        self.posicion_2[1] = event.pos().y()
+
+        self.update()
+    
+    def paintEvent(self, event):
+        if self.tipo_linea is not None:
+            painter = QPainter()
+            painter.begin(self)
+
+            pincel = QPen(Qt.red, 7)
+            pincel.setStyle(self.tipos_lineas[self.tipo_linea])
+
+            painter.setPen(pincel)
+            painter.drawLine(self.posicion_1[0], self.posicion_1[1], self.posicion_2[0], self.posicion_2[1])
+
+            painter.end()
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = AplicacionDibujoLineas()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
