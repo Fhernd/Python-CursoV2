901836b2ddc72b1abd94cdb201973bce20ba691a
infzer0 <johnortizo@outlook.com>
2020-07-31 09:46:36 -0500

    Python Curso V2: 564 2/2 Dibujar una Figura Tipo Círculo sobre una Ventana usando el Mouse
    Dibujar una Figura Tipo Círculo sobre una Ventana usando el Mouse con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo93_aplicacion_dibujo_circulo.py
+++ parte17/demo93_aplicacion_dibujo_circulo.py
@@ -17,7 +17,34 @@ class AplicacionDibujoCirculo(QMainWindow):
         self.ui = Ui_DibujoCirculo()
         self.ui.setupUi(self)
 
+        self.posicion_1 = [0, 0]
+        self.posicion_2 = [0, 0]
+
         self.show()
+    
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            self.posicion_1[0] = event.pos().x()
+            self.posicion_1[1] = event.pos().y()
+    
+    def mouseReleaseEvent(self, event):
+        self.posicion_2[0] = event.pos().x()
+        self.posicion_2[1] = event.pos().y()
+
+        self.update()
+    
+    def paintEvent(self, event):
+        ancho = self.posicion_2[0] - self.posicion_1[0]
+        alto = self.posicion_2[1] - self.posicion_1[1]
+
+        painter = QPainter()
+        painter.begin(self)
+
+        rectangulo = QRect(self.posicion_1[0], self.posicion_1[1], ancho, alto)
+
+        painter.drawArc(rectangulo, 0, 360 * 16)
+
+        painter.end()
 
 def main():
     app = QApplication(sys.argv)
