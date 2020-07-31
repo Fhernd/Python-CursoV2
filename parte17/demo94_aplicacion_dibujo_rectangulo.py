773d2767abb192e3674e99e0978cbb4397d532b3
infzer0 <johnortizo@outlook.com>
2020-07-31 14:54:49 -0500

    Python Curso V2: 566 2/2 Dibujar una Figura Tipo Rectángulo sobre una Ventana usando el Mouse
    Dibujar una Figura Tipo Rectángulo sobre una Ventana usando el Mouse con el lenguaje de programación Python y la librería PyQt5.

--- parte17/demo94_aplicacion_dibujo_rectangulo.py
+++ parte17/demo94_aplicacion_dibujo_rectangulo.py
@@ -6,4 +6,48 @@ from PyQt5.QtGui import QPainter, QPen
 from demo94_dibujo_rectangulo import Ui_DibujoRectangulo
 
 class AplicacionDibujoRectangulo(QMainWindow):
-    pass
+    
+    def __init__(self):
+        super().__init__()
+
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        self.ui = Ui_DibujoRectangulo()
+        self.ui.setupUi(self)
+
+        self.posicion_1 = [0, 0]
+        self.posicion_2 = [0, 0]
+
+        self.show()
+
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            self.posicion_1[0] = event.pos().x()
+            self.posicion_1[1] = event.pos().y()
+    
+    def mouseReleaseEvent(self, event):
+        self.posicion_2[0] = event.pos().x()
+        self.posicion_2[1] = event.pos().y()
+
+        self.update()
+
+    def paintEvent(self, event):
+        ancho = self.posicion_2[0] - self.posicion_1[0]
+        alto = self.posicion_2[1] - self.posicion_1[1]
+
+        painter = QPainter()
+        painter.begin(self)
+
+        painter.drawRect(self.posicion_1[0], self.posicion_1[1], ancho, alto)
+
+        painter.end()
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = AplicacionDibujoRectangulo()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
