3270cd74c9d63638afcd3dff9bea745f5aae7237
infzer0 <johnortizo@outlook.com>
2020-09-02 12:58:45 -0500

    Python Curso V2: 581 Ejercicio 1 - Variables Extras para Dibujar y Sobrescritura del Método mousePressEvent()
    Ejercicio 1 - Variables Extras para Dibujar y Sobrescritura del Método mousePressEvent() con el lenguaje de programación Python.

--- parte17/ex1_aplicacion_graficos.py
+++ parte17/ex1_aplicacion_graficos.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
 from ex1_graficos import Ui_Graficos
 
 class GraficosAplicacion(QMainWindow):
@@ -14,7 +15,29 @@ class GraficosAplicacion(QMainWindow):
         self.ui = Ui_Graficos()
         self.ui.setupUi(self)
 
+        self.ui.mniDibujarCirculo.triggered.connect(self.dibujar_circulo)
+        self.ui.mniDibujarRectangulo.triggered.connect(self.dibujar_rectangulo)
+        self.ui.mniDibujarLinea.triggered.connect(self.dibujar_linea)
+
+        self.posicion_1 = [0, 0]
+        self.posicion_2 = [0, 0]
+        self.tipo_figura = None
+
         self.show()
+
+    def dibujar_circulo(self):
+        self.tipo_figura = 'circulo'
+
+    def dibujar_rectangulo(self):
+        self.tipo_figura = 'rectangulo'
+
+    def dibujar_linea(self):
+        self.tipo_figura = 'linea'
+    
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            self.posicion_1[0] = event.pos().x()
+            self.posicion_1[1] = event.pos().y()
 
 def main():
     app = QApplication(sys.argv)
