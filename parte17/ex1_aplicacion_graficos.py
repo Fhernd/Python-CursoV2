79c619e1b4aae8c11bdca7395ea9fe126a7f5d8a
infzer0 <johnortizo@outlook.com>
2020-09-03 15:45:58 -0500

    Python Curso V2: 583 Ejercicio 1 - Terminación de la Aplicación de Dibujo de Figuras Geométricas
    Ejercicio 1 - Terminación de la Aplicación de Dibujo de Figuras Geométricas con el lenguaje de programación Python.

--- parte17/ex1_aplicacion_graficos.py
+++ parte17/ex1_aplicacion_graficos.py
@@ -1,7 +1,8 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import Qt, QRect
 from ex1_graficos import Ui_Graficos
 
 class GraficosAplicacion(QMainWindow):
@@ -23,6 +24,9 @@ class GraficosAplicacion(QMainWindow):
         self.posicion_2 = [0, 0]
         self.tipo_figura = None
 
+        self.mensaje = QMessageBox(self)
+        self.setWindowTitle('Mensaje')
+
         self.show()
 
     def dibujar_circulo(self):
@@ -38,6 +42,34 @@ class GraficosAplicacion(QMainWindow):
         if event.buttons() & Qt.LeftButton:
             self.posicion_1[0] = event.pos().x()
             self.posicion_1[1] = event.pos().y()
+    
+    def mouseReleaseEvent(self, event):
+        self.posicion_2[0] = event.pos().x()
+        self.posicion_2[1] = event.pos().y()
+
+        self.update()
+
+    def paintEvent(self, event):
+        if self.tipo_figura:
+            painter = QPainter()
+            painter.begin(self)
+
+            if self.tipo_figura == 'circulo':
+                ancho = self.posicion_2[0] - self.posicion_1[0]
+                alto = self.posicion_2[1] - self.posicion_1[1]
+
+                rectangulo = QRect(self.posicion_1[0], self.posicion_1[1], ancho, alto)
+                angulo_inicio = 0
+                arco = 360 * 16
+                painter.drawArc(rectangulo, angulo_inicio, arco)
+                painter.end()
+            if self.tipo_figura == 'rectangulo':
+                ancho = self.posicion_2[0] - self.posicion_1[0]
+                alto = self.posicion_2[1] - self.posicion_1[1]
+
+                painter.drawRect(self.posicion_1[0], self.posicion_1[1], ancho, alto)
+            if self.tipo_figura == 'linea':
+                painter.drawLine(self.posicion_1[0], self.posicion_1[1], self.posicion_2[0], self.posicion_2[1])
 
 def main():
     app = QApplication(sys.argv)
