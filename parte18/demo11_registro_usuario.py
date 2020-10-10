890b0cd12ee0ee13925967de6e97696145f57467
infzer0 <johnortizo@outlook.com>
2020-10-10 09:14:09 -0500

    Lodash Ejercicio: 162 Usar la Función isSafeInteger() para Validar Si un Argumento es un Entero Seguro
    Usar la Función isSafeInteger() para Validar Si un Argumento es un Entero Seguro con el lenguaje de programación JavaScript.

--- parte18/demo11_registro_usuario.py
+++ parte18/demo11_registro_usuario.py
@@ -1,3 +1,4 @@
+import re
 import tkinter as tk
 from tkinter import messagebox
 
@@ -7,17 +8,22 @@ class VentanaPrincipal(tk.Frame):
         self.master = master
         self.pack()
 
+        patron = r'^[a-z]+@[a-z]+\.[a-z]{2,3}$'
+        self.regex_correo = re.compile(patron)
+
         self.inicializar_gui()
     
     def inicializar_gui(self):
-        lbl_nombre = tk.Label(self, 'Nombre: ')
+        lbl_nombre = tk.Label(self, text='Nombre: ')
         lbl_nombre.grid(row=0, column=0)
-        self.txt_nombre = tk.Entry(self)
+        self.nombre = tk.StringVar()
+        self.txt_nombre = tk.Entry(self, textvariable=self.nombre)
         self.txt_nombre.grid(row=0, column=1)
 
-        lbl_correo = tk.Label(self, 'Correo: ')
+        lbl_correo = tk.Label(self, text='Correo: ')
         lbl_correo.grid(row=1, column=0)
-        self.txt_correo = tk.Entry(self)
+        self.correo = tk.StringVar()
+        self.txt_correo = tk.Entry(self, textvariable=self.correo)
         self.txt_correo.grid(row=1, column=1)
 
         self.registro_valido = tk.IntVar()
@@ -29,9 +35,33 @@ class VentanaPrincipal(tk.Frame):
         btn_validar_registro.grid(row=3, column=0)
     
     def validar_registro(self):
-        nombre = self.txt_correo.get().strip()
+        nombre = self.txt_nombre.get().strip()
         correo = self.txt_correo.get().strip()
-
+        
         if len(nombre) == 0:
             messagebox.showwarning('Mensaje', 'El campo Nombre es obligatorio.')
             return
+
+        if len(correo) == 0:
+            messagebox.showwarning('Mensaje', 'El campo Correo es obligatorio.')
+            return
+        
+        if self.regex_correo.match(correo) is None:
+            messagebox.showwarning('Mensaje', 'El campo Correo debe ser una dirección válida.')
+            return
+        
+        if self.registro_valido.get():
+            messagebox.showinfo('Mensaje', 'El registro ha sido satisfactorio.')
+        else:
+            messagebox.showwarning('Mensaje', 'Debe aceptar los Términos y Condiciones para completar el registro.')
+
+def main():
+    app = tk.Tk()
+    app.title('Registro de Usuarios')
+    app.geometry('400x300')
+
+    ventana = VentanaPrincipal(app)
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
