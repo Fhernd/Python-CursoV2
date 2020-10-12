643a4aac48a2be2f96fdabe5ed42ccc36997bdb2
infzer0 <johnortizo@outlook.com>
2020-10-12 18:16:50 -0500

    Python Curso V2: 632 2/2 Usar el Componente Progressbar de Tkinter para Mostrar una Barra de Progreso
    Uso Componente Gráfico Listbox para Cambiar Color Fondo de Ventana con el lenguaje de programación Python.

--- parte18/demo14_lista_elementos_listbox.py
+++ parte18/demo14_lista_elementos_listbox.py
@@ -19,11 +19,29 @@ class VentanaPrincipal(tk.Frame):
         self.lbx_colores.bind('<Double-1>', self.cambiar_color)
         self.lbx_colores.pack()
 
-        self.lbl_color_seleccionado = tk.Label(self, text='Blanco')
+        self.color_seleccionado = tk.StringVar()
+        self.lbl_color_seleccionado = tk.Label(self, text='Blanco', textvariable=self.color_seleccionado)
         self.lbl_color_seleccionado.pack()
 
-    def cambiar_color(self):
-        pass
+    def cambiar_color(self, evento):
+        color_seleccionado = self.lbx_colores.curselection()
+
+        for c in color_seleccionado:
+            if c == 0:
+                self.master.configure(background='red')
+                self.color_seleccionado.set('Rojo')
+            elif c == 1:
+                self.master.configure(background='green')
+                self.color_seleccionado.set('Verde')
+            elif c == 2:
+                self.master.configure(background='blue')
+                self.color_seleccionado.set('Azul')
+            elif c == 3:
+                self.master.configure(background='yellow')
+                self.color_seleccionado.set('Amarillo')
+            elif c == 4:
+                self.master.configure(background='white')
+                self.color_seleccionado.set('Blanco')
 
 def main():
     app = tk.Tk()
