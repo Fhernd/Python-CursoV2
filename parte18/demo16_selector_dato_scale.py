76665a7cec970d4f29e0211cc6865216956539e5
infzer0 <johnortizo@outlook.com>
2020-10-14 12:10:31 -0500

    Python Curso V2: 636 1/2 Componente Gráfico Scale de Tkinter para Selector de Valores Numéricos
    Componente Gráfico Scale de Tkinter para Selector de Valores Numéricos con el lenguaje de programación Python.

---
+++ parte18/demo16_selector_dato_scale.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+class SeleccionDato(tk.Frame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        self.valor_seleccionado = tk.DoubleVar()
+
+        self.scl_dato = tk.Scale(self, variable=self.valor_seleccionado, from_=1, to=100, orient=tk.HORIZONTAL)
+        self.scl_dato.pack()
+
+        tk.Label(self, text='Scale horizontal').pack()
+
+        tk.Button(self, text='Seleccionar valor', command=self.seleccionar_valor).pack()
+
+        self.lbl_valor_seleccionado = tk.Label(self).pack()
+    
+    def seleccionar_valor(self):
+        pass
+
+def main():
+    app = tk.Tk()
+    app.title('Selector de Datos')
+    app.geometry('350x300')
+
+    ventana = SeleccionDato(app)
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
