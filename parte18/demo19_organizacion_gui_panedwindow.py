d7254eb8baed204c5df429d9d071fbcc02e961c2
infzer0 <johnortizo@outlook.com>
2020-10-23 19:42:09 -0500

    Python Curso V2: 643 2/2 Organizar una Interfaz Gráfica de Usuario con un Componente PanedWindow
    Organizar una Interfaz Gráfica de Usuario con un Componente PanedWindow con el lenguaje de programación Python.

--- parte18/demo19_organizacion_gui_panedwindow.py
+++ parte18/demo19_organizacion_gui_panedwindow.py
@@ -10,18 +10,31 @@ class OrganizacionInterfaz(tk.Frame):
         self.inicializar_gui()
     
     def inicializar_gui(self):
-        organizador = tk.PanedWindow(self, orient='vertical')
+        organizador = tk.PanedWindow(self.master, orient='vertical')
 
         btn_mostrar_mensaje = tk.Button(organizador, text='Mostrar mensaje')
         btn_mostrar_mensaje.pack(side=tk.TOP)
         organizador.add(btn_mostrar_mensaje)
-
         organizador.pack(fill=tk.BOTH, expand=True)
+
+        chk_activar_accion = tk.Checkbutton(organizador, text='Activar accion')
+        chk_activar_accion.pack(side=tk.TOP)
+        organizador.add(chk_activar_accion)
+
+        lbl_saludo = tk.Label(organizador, text='¡Tkinter es tremendo!')
+        lbl_saludo.pack(side=tk.TOP)
+        organizador.add(lbl_saludo)
+
+        txt_nombre = tk.Entry(organizador, text='¡Python es genial!')
+        txt_nombre.pack(side=tk.TOP)
+        organizador.add(txt_nombre)
+
+        organizador.configure(sashrelief=tk.RAISED)
 
 def main():
     app = tk.Tk()
     app.title('Interfaz Organizada')
-    app.geometry('300x300')
+    app.geometry('300x150')
 
     ventana = OrganizacionInterfaz(app)
     ventana.mainloop()
