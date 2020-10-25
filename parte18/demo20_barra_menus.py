d042c6dda68c87567128aec4e830ab8b3aa57304
infzer0 <johnortizo@outlook.com>
2020-10-24 19:57:11 -0500

    Python Curso V2: 644 1/2 Crear una Barra de Menú sobre la Ventana Principal de una Aplicación Tkinter
    Crear una Barra de Menú sobre la Ventana Principal de una Aplicación Tkinter con el lenguaje de programación Python.

---
+++ parte18/demo20_barra_menus.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+class Aplicacion(tk.Frame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        mbr_principal = tk.Menu(self.master)
+        mnu_archivo = tk.Menu(mbr_principal)
+
+        mbr_principal.add_cascade(label='Archivo')
+
+        self.master.config(menu=mbr_principal)
+
+def main():
+    ventana_principal = tk.Tk()
+    ventana_principal.title('Barra de menu')
+    ventana_principal.geometry('350x300')
+
+    app = Aplicacion(ventana_principal)
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
