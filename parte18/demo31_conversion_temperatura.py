0c26eeb146a496321657ceea7d49d131ce0127e6
infzer0 <johnortizo@outlook.com>
2020-11-10 16:13:29 -0500

    Python Curso V2: 659 2/2 Aplicación Tkinter para Convertir de Grados Fahrenheit en Grados Celsius
    Aplicación Tkinter para Convertir de Grados Fahrenheit en Grados Celsius con el lenguaje de programación Python.

--- parte18/demo31_conversion_temperatura.py
+++ parte18/demo31_conversion_temperatura.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class ConversionTemperatura(tk.Frame):
 
@@ -10,15 +11,26 @@ class ConversionTemperatura(tk.Frame):
     
     def inicializar_gui(self):
         self.txt_fahrenheit = tk.Entry(self.master, width=10)
-        self.txt_fahrenheit.grid(row=0, column=0, sticky='e')
+        self.txt_fahrenheit.grid(row=0, column=0, sticky='e', padx=10, pady=10)
 
         lbl_fahrenheit = tk.Label(self.master, text="\N{DEGREE FAHRENHEIT}")
-        lbl_fahrenheit.grid(row=0, column=1, sticky='w')
+        lbl_fahrenheit.grid(row=0, column=1, sticky='w', padx=10, pady=10)
 
         btn_convertir = tk.Button(self.master, text='\N{RIGHTWARDS BLACK ARROW}', command=self.convertir)
+        btn_convertir.grid(row=0, column=2, padx=10, pady=10)
+
+        self.lbl_celsius = tk.Label(self.master, text='\N{DEGREE CELSIUS}')
+        self.lbl_celsius.grid(row=0, column=3, padx=10, pady=10)
     
     def convertir(self):
-        pass
+        try:
+            fahrenheit = float(self.txt_fahrenheit.get())
+
+            celsius = (5/9) * (fahrenheit - 32)
+
+            self.lbl_celsius['text'] = f'{round(celsius, 2)} \N{DEGREE CELSIUS}'
+        except:
+            messagebox.showwarning('Advertencia', 'Debe escribir un número en el campo \N{DEGREE FAHRENHEIT}.')
 
 def main():
     root = tk.Tk()
@@ -27,3 +39,6 @@ def main():
 
     ventana = ConversionTemperatura(root)
     ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
