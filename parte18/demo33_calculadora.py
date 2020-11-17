869e6ba9a8d3f1477dfdcac2e301a4a914d63b47
infzer0 <johnortizo@outlook.com>
2020-11-17 18:58:29 -0500

    Python Curso V2: 665 3/3 Aplicación Calculadora de Aritmética Básica con Tkinter
    Aplicación Calculadora de Aritmética Básica con Tkinter con el lenguaje de programación Python.

--- parte18/demo33_calculadora.py
+++ parte18/demo33_calculadora.py
@@ -29,20 +29,87 @@ class CalculadoraAplicacion:
         
         btn_multiplicar = tk.Button(self.root, text='Multiplicar', command=self.multiplicar, width=10)
         btn_multiplicar.place(x=160, y=90)
+        
+        btn_dividir = tk.Button(self.root, text='Dividir', command=self.dividir, width=10)
+        btn_dividir.place(x=245, y=90)
+
+        lbl_resultado = tk.Label(self.root, text='Resultado: ')
+        lbl_resultado.place(x=20, y=170)
+
+        self.txt_resultado = tk.Entry(self.root)
+        self.txt_resultado.place(x=200, y=170)
     
     def sumar(self):
-        pass
+        try:
+            primer_numero = float(self.txt_primer_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Primer número debe ser un valor numérico.')
+        
+        try:
+            segundo_numero = float(self.txt_segundo_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Segundo número debe ser un valor numérico.')
+
+        suma = primer_numero + segundo_numero
+
+        self.txt_resultado.delete(0, tk.END)
+        self.txt_resultado.insert(tk.END, str(suma))
     
     def restar(self):
-        pass
+        try:
+            primer_numero = float(self.txt_primer_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Primer número debe ser un valor numérico.')
+        
+        try:
+            segundo_numero = float(self.txt_segundo_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Segundo número debe ser un valor numérico.')
+
+        resta = primer_numero - segundo_numero
+
+        self.txt_resultado.delete(0, tk.END)
+        self.txt_resultado.insert(tk.END, str(resta))
     
     def multiplicar(self):
-        pass
+        try:
+            primer_numero = float(self.txt_primer_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Primer número debe ser un valor numérico.')
+        
+        try:
+            segundo_numero = float(self.txt_segundo_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Segundo número debe ser un valor numérico.')
+
+        multiplicacion = primer_numero * segundo_numero
+
+        self.txt_resultado.delete(0, tk.END)
+        self.txt_resultado.insert(tk.END, str(multiplicacion))
+
+    def dividir(self):
+        try:
+            primer_numero = float(self.txt_primer_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Primer número debe ser un valor numérico.')
+        
+        try:
+            segundo_numero = float(self.txt_segundo_numero.get())
+        except Exception:
+            messagebox.showwarning('Mensaje', 'El campo Segundo número debe ser un valor numérico.')
+
+        if segundo_numero != 0:
+            division = primer_numero / segundo_numero
+
+            self.txt_resultado.delete(0, tk.END)
+            self.txt_resultado.insert(tk.END, str(division))
+        else:
+            messagebox.showwarning('Mensaje', 'El campo Segundo número no puede ser cero (0).')
 
 def main():
     root = tk.Tk()
     root.title('Calculadora')
-    root.geometry('350x300')
+    root.geometry('350x250')
 
     calculadora = CalculadoraAplicacion(root)
     
