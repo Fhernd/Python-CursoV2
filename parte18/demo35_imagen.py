1807f78ceb7b6b609f186385bbaa8dc2cca8c178
infzer0 <johnortizo@outlook.com>
2020-11-20 12:18:15 -0500

    Python Curso V2: 668 1/2 Mostrar una Imagen sobre una Ventana Construida con Tkinter
    Mostrar una Imagen sobre una Ventana Construida con Tkinter con el lenguaje de programación Python.

---
+++ parte18/demo35_imagen.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+class VisualizacionImagen:
+
+    def __init__(self, master):
+        self.master = master
+
+        self.inicializar_gui()
+
+    def inicializar_gui(self):
+        canvas = tk.Canvas(self.master, width=920, height=500)
+        canvas.pack()
+
+        img_logo_python = tk.PhotoImage(file='parte18/python-logo.png')
+        canvas.create_image(0, 0, anchor=tk.NW, image=img_logo_python)
+
+def main():
+    root = tk.Tk()
+    root.title('Logo Python')
+    root.geometry('920x500')
+
+    ventana = VisualizacionImagen(root)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
