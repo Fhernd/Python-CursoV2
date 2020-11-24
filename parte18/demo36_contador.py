661125a93e23fd19c3953b94f563d9155b0a92de
infzer0 <johnortizo@outlook.com>
2020-11-24 09:30:30 -0500

    Python Curso V2: 671 2/2 Crear una Interfaz Gráfica para Controlar un Contador Numérico
    Crear una Interfaz Gráfica para Controlar un Contador Numérico con el lenguaje de programación Python.

--- parte18/demo36_contador.py
+++ parte18/demo36_contador.py
@@ -13,13 +13,25 @@ class Contador:
 
         btn_reducir = tk.Button(self.master, text='-', command=self.reducir)
         btn_reducir.grid(row=0, column=0, sticky='nsew')
+
+        self.lbl_contador = tk.Label(master=self.master, text='0')
+        self.lbl_contador.grid(row=0, column=1)
+
+        btn_aumentar = tk.Button(master=self.master, text='+', command=self.aumentar)
+        btn_aumentar.grid(row=0, column=2, sticky='nsew')
     
     def reducir(self):
-        pass
+        valor = int(self.lbl_contador['text'])
+        self.lbl_contador['text'] = f'{valor - 1}'
+
+    def aumentar(self):
+        valor = int(self.lbl_contador['text'])
+        self.lbl_contador['text'] = f'{valor + 1}'
 
 def main():
     root = tk.Tk()
     root.title('Controles Reducción/Aumento')
+    root.geometry('300x100')
     
     contador = Contador(root)
     root.mainloop()
