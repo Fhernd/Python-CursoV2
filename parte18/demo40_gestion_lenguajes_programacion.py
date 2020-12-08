b23c767908492c687c8e59ea59f3b392ce60e3eb
infzer0 <johnortizo@outlook.com>
2020-12-07 19:29:33 -0500

    Python Curso V2: 679 2/3 Gestor de Lenguajes de Programación con una GUI Creada en Tkinter
    Gestor de Lenguajes de Programación con una GUI Creada en Tkinter con el lenguaje de programación Python.

--- parte18/demo40_gestion_lenguajes_programacion.py
+++ parte18/demo40_gestion_lenguajes_programacion.py
@@ -14,6 +14,29 @@ class GestionLenguajesProgramacion:
         self.lst_lenguajes = tk.Listbox(self.master, width=16, height=18)
         self.lst_lenguajes.place(x=20, y=20)
 
+        btn_agregar_lenguaje_arriba = tk.Button(self.master, text='Agregar lenguaje parte superior')
+        btn_agregar_lenguaje_arriba['command'] = self.agregar_lenguaje_arriba
+        btn_agregar_lenguaje_arriba.place(x=160, y=20)
+
+        btn_agregar_lenguaje_abajo = tk.Button(self.master, text='Agregar lenguaje parte abajo')
+        btn_agregar_lenguaje_abajo['command'] = self.agregar_lenguaje_abajo
+        btn_agregar_lenguaje_abajo.place(x=160, y=50)
+
+        btn_eliminar_lenguaje_seleccionado = tk.Button(self.master, text='Eliminar lenguaje seleccionado')
+        btn_eliminar_lenguaje_seleccionado['command'] = self.eliminar_lenguaje_seleccionada
+        btn_eliminar_lenguaje_seleccionado.place(x=160, y=50)
+    
+    def agregar_lenguaje_arriba(self):
+        lenguaje = simpledialog.askstring('Agregar lenguaje', 'Escriba el lenguaje de programación a agregar')
+
+        self.lst_lenguajes.insert(0, lenguaje)
+
+    def agregar_lenguaje_abajo(self):
+        pass
+    
+    def eliminar_lenguaje_seleccionada(self):
+        pass
+
 def main():
     master = tk.Tk()
     master.title('Gestión Lenguajes de Programación')
