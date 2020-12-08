0a4b6f5cadfb522f17a8b7e48f87bbb9e803d74f
infzer0 <johnortizo@outlook.com>
2020-12-08 13:39:04 -0500

    Python Curso V2: 680 3/3 Gestor de Lenguajes de Programación con una GUI Creada en Tkinter
    Gestor de Lenguajes de Programación con una GUI Creada en Tkinter con el lenguaje de programación Python.

--- parte18/demo40_gestion_lenguajes_programacion.py
+++ parte18/demo40_gestion_lenguajes_programacion.py
@@ -13,6 +13,9 @@ class GestionLenguajesProgramacion:
     def inicializar_gui(self):
         self.lst_lenguajes = tk.Listbox(self.master, width=16, height=18)
         self.lst_lenguajes.place(x=20, y=20)
+        self.lst_lenguajes.bind('<<ListboxSelect>>', self.seleccionar_lenguaje)
+
+        self.lenguaje_seleccionado = -1
 
         btn_agregar_lenguaje_arriba = tk.Button(self.master, text='Agregar lenguaje parte superior')
         btn_agregar_lenguaje_arriba['command'] = self.agregar_lenguaje_arriba
@@ -24,7 +27,7 @@ class GestionLenguajesProgramacion:
 
         btn_eliminar_lenguaje_seleccionado = tk.Button(self.master, text='Eliminar lenguaje seleccionado')
         btn_eliminar_lenguaje_seleccionado['command'] = self.eliminar_lenguaje_seleccionada
-        btn_eliminar_lenguaje_seleccionado.place(x=160, y=50)
+        btn_eliminar_lenguaje_seleccionado.place(x=160, y=80)
     
     def agregar_lenguaje_arriba(self):
         lenguaje = simpledialog.askstring('Agregar lenguaje', 'Escriba el lenguaje de programación a agregar')
@@ -32,10 +35,19 @@ class GestionLenguajesProgramacion:
         self.lst_lenguajes.insert(0, lenguaje)
 
     def agregar_lenguaje_abajo(self):
-        pass
+        lenguaje = simpledialog.askstring('Agregar lenguaje', 'Escriba el lenguaje de programación a agregar')
+
+        self.lst_lenguajes.insert(tk.END, lenguaje)
     
     def eliminar_lenguaje_seleccionada(self):
-        pass
+        if self.lenguaje_seleccionado >= 0:
+            self.lst_lenguajes.delete(self.lenguaje_seleccionado)
+            self.lenguaje_seleccionado = -1
+        else:
+            messagebox.showwarning('Mensaje', 'Antes de eliminar debe seleccionar un elemento de la lista.')
+
+    def seleccionar_lenguaje(self, evento):
+        self.lenguaje_seleccionado = int(self.lst_lenguajes.curselection()[0])
 
 def main():
     master = tk.Tk()
