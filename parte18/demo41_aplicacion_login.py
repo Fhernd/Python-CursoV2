6e52b5ae7671f518b315d2c5404d6c49e5fbdfb5
infzer0 <johnortizo@outlook.com>
2020-12-12 10:35:40 -0500

    Python Curso V2: 682 2/4 Ventana Emergente para Iniciar Sesión en una Aplicación Tkinter
    Ventana Emergente para Iniciar Sesión en una Aplicación Tkinter con el lenguaje de programación Python.

--- parte18/demo41_aplicacion_login.py
+++ parte18/demo41_aplicacion_login.py
@@ -12,14 +12,32 @@ class Aplicacion:
         self.inicializar_gui()
     
     def inicializar_gui(self):
-        lbl_bienvenido = tk.Label(self.master, text='¡Bienvenido!', font=('Helvetica', 43))
-        lbl_bienvenido.place(x=140, y=120)
+        lbl_bienvenido = tk.Label(self.master, text='¡Bienvenido!', font=('Helvetica', 27))
+        lbl_bienvenido.place(x=80, y=90)
 
         btn_login = tk.Button(self.master, text='Login')
         btn_login['command'] = self.login
+        btn_login.place(x=150, y=180)
     
     def login(self):
         pass
+
+
+class LoginVentana:
+    def __init__(self, master):
+        self.master = master
+        self.ventana = tk.Toplevel()
+        self.ventana.title('Login')
+        self.ventana.geometry('300x200')
+
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        lbl_usuario = tk.Label(self.ventana, text='Usuario:')
+        lbl_usuario.place(x=20, y=20)
+        txt_usuario = tk.Entry(self.ventana)
+        txt_usuario.place(x=80, y=20)
+        txt_usuario.focus()
 
 def main():
     root = tk.Tk()
