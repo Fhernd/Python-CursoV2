e8fe453ec0e4aec166ff18c8c8e61331ab16ec62
infzer0 <johnortizo@outlook.com>
2020-12-16 04:46:49 -0500

    Python Curso V2: 686 2/6 Aplicación de Gestión de Contactos con Interfaz Gráfica de Usuario Tkinter
    Aplicación de Gestión de Contactos con Interfaz Gráfica de Usuario Tkinter con el lenguaje de programación Python.

--- parte18/demo42_app_contactos.py
+++ parte18/demo42_app_contactos.py
@@ -8,9 +8,9 @@ from PIL import Image, ImageTk
 
 class Contacto:
     
-    def __init__(self, nombre, email, fecha_nacimiento):
-        self.nombre = nombre
+    def __init__(self, email, nombre, fecha_nacimiento):
         self.email = email
+        self.nombre = nombre
         self.fecha_nacimiento = fecha_nacimiento
     
     def __str__(self):
@@ -27,3 +27,54 @@ class GestionContactos:
         if not os.path.exists(self.nombre_archivo):
             with open(self.nombre_archivo, 'wt', encoding='utf-8') as f:
                 pass
+    
+    def agregar_contacto(self, contacto):
+        try:
+            with open(self.nombre_archivo, 'at', encoding='utf-8') as f:
+                f.write(contacto)
+                f.write('\n')
+
+            return True
+        except:
+            return False
+    
+    def existe_contacto(self, email):
+        for c in self.obtener_contactos:
+            if c.email == email:
+                return True
+        
+        return False
+    
+    def obtener_contactos(self):
+        try:
+            contactos = []
+            with open(self.nombre_archivo, 'rt', encoding='utf-8') as f:
+                for l in f.readlines():
+                    partes = l.split(';')
+                    contacto = Contacto(*partes)
+                    contactos.append(contacto)
+
+            return contactos
+        except:
+            return None
+    
+    def buscar_contacto_por_email(self, email):
+        for c in self.obtener_contactos:
+            if c.email == email:
+                return c
+        
+        return None
+    
+    def eliminar_contacto_por_email(self, email):
+        contactos = self.obtener_contactos
+
+        for c in contactos:
+            if c.email == email:
+                contactos.remove(c)
+                reemplazar_archivo(contactos)
+                return True
+        
+        return False
+    
+    def reemplazar_archivo(self, contactos):
+        pass
