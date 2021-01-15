2c8cdf6a8921051645bc4c1e15eb95a7a5b7a3b1
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-15 16:56:49 -0500

    Python Curso V2: 693 3/5 Aplicación de Calculadora con una GUI Construida Bajo Tkinter
    Aplicación de Calculadora con una GUI Construida Bajo Tkinter con el lenguaje de programación Python.

--- parte18/demo43_calculadora.py
+++ parte18/demo43_calculadora.py
@@ -30,7 +30,58 @@ class CalculadoraApp:
         btn_limpiar['command'] = self.limpiar
         btn_limpiar.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
 
+        btn_dividir = Button(frm_botones, text='/', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_dividir['cursor'] = 'hand2'
+        btn_dividir['command'] = self.dividir
+        btn_dividir.grid(row=0, column=3, padx=1, pady=1)
+
+        btn_siete = Button(frm_botones, text='7', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_siete['cursor'] = 'hand2'
+        btn_siete['command'] = lambda: self.presion_tecla(7)
+        btn_siete.grid(row=1, column=0, padx=1, pady=1)
+        
+        btn_ocho = Button(frm_botones, text='8', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_ocho['cursor'] = 'hand2'
+        btn_ocho['command'] = lambda: self.presion_tecla(8)
+        btn_ocho.grid(row=1, column=1, padx=1, pady=1)
+        
+        btn_nueve = Button(frm_botones, text='9', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_nueve['cursor'] = 'hand2'
+        btn_nueve['command'] = lambda: self.presion_tecla(9)
+        btn_nueve.grid(row=1, column=2, padx=1, pady=1)
+        
+        btn_producto = Button(frm_botones, text='*', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_producto['cursor'] = 'hand2'
+        btn_producto['command'] = lambda: self.presion_tecla('*')
+        btn_producto.grid(row=1, column=3, padx=1, pady=1)
+        
+        btn_cuatro = Button(frm_botones, text='4', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_cuatro['cursor'] = 'hand2'
+        btn_cuatro['command'] = lambda: self.presion_tecla(4)
+        btn_cuatro.grid(row=2, column=0, padx=1, pady=1)
+        
+        btn_ocho = Button(frm_botones, text='5', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_ocho['cursor'] = 'hand2'
+        btn_ocho['command'] = lambda: self.presion_tecla(5)
+        btn_ocho.grid(row=2, column=1, padx=1, pady=1)
+        
+        btn_nueve = Button(frm_botones, text='6', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_nueve['cursor'] = 'hand2'
+        btn_nueve['command'] = lambda: self.presion_tecla(6)
+        btn_nueve.grid(row=2, column=2, padx=1, pady=1)
+        
+        btn_producto = Button(frm_botones, text='-', fg='black', width=10, height=3, bd=0, bg='#EEE')
+        btn_producto['cursor'] = 'hand2'
+        btn_producto['command'] = lambda: self.presion_tecla('-')
+        btn_producto.grid(row=2, column=3, padx=1, pady=1)
+
     def limpiar(self):
+        pass
+    
+    def dividir(self):
+        pass
+
+    def presion_tecla(self, tecla):
         pass
 
 def main():
