0c30bfa91c502088b6bc2ebaf6f1366b59a20563
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-20 12:15:27 -0500

    Python Curso V2: 697 2/2 Aplicación para Modificar el Tamaño de un Círculo con un Control Slider
    Aplicación para Modificar el Tamaño de un Círculo con un Control Slider con el lenguaje de programación Python.

--- parte18/demo44_circulo.py
+++ parte18/demo44_circulo.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 
 class CirculoEscalado(Frame):
 
@@ -7,12 +7,18 @@ class CirculoEscalado(Frame):
         
         self.pack(expand=YES, fill=BOTH)
         self.master.title('Círculo - Escalado')
-        self.master.geomtry('220x280')
+        self.master.geometry('220x280')
 
-        self.sdr_escalado = Scale(self, from_=0, to=200, orient=HORIZONTAL, command=self.escalar)
+        self.sdr_escalado = Scale(self, from_=0, to=1000, orient=HORIZONTAL, command=self.escalar)
+        self.sdr_escalado.pack(side=BOTTOM, fill=X)
+
+        self.canvas = Canvas(self, bg='white')
+        self.canvas.pack(expand=YES, fill=BOTH)
     
     def escalar(self, valor):
-        pass
+        posicion = int(valor) + 10
+        self.canvas.delete('circulo')
+        self.canvas.create_oval(10, 10, posicion, posicion, fill='yellow', tags='circulo')
 
 def main():
     CirculoEscalado().mainloop()
