c012f80785099f542b6763bde84a02f0406e8ec2
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-21 16:04:33 -0500

    Python Curso V2: 699 2/2 Dibujar un Plot para Gráficas sobre un Componente Canvas de Tkinter
    Dibujar un Plot para Gráficas sobre un Componente Canvas de Tkinter con el lenguaje de programación Python.

--- parte18/demo45_plot.py
+++ parte18/demo45_plot.py
@@ -1,4 +1,4 @@
-from tkinter import Button, Canvas, Tk
+from tkinter import Button, Canvas, E, N, Tk
 
 class Plot(object):
 
@@ -14,6 +14,30 @@ class Plot(object):
         btn_salir = Button(self.master, text='Salir')
         btn_salir['command'] = self.salir
         btn_salir.pack()
+
+        canvas.create_line(100, 250, 400, 250, width=2)
+        canvas.create_line(100, 250, 100, 50, width=2)
+
+        for i in range(11):
+            x = 100 + (i * 30)
+            canvas.create_line(x, 250, x, 245, width=2)
+            canvas.create_text(x, 254, text='%d' % (10.0 * i), anchor=N)
+
+        for i in range(6):
+            y = 250 - (i * 40)
+            canvas.create_line(100, y, 105, y, width=2)
+            canvas.create_text(96, y, text='%5.1f' % (50.0 * i), anchor=E)
+
+        escalado = []
+        puntos = [(12, 56), (20, 94), (33, 98), (45, 120), (61, 180), (75, 160), (98, 223)]
+
+        for x, y in puntos:
+            escalado.append((100 + 3 * x, 250 - (4*y)/5))
+
+        canvas.create_line(escalado, fill='black', smooth=1)
+
+        for x, y in escalado:
+            canvas.create_oval(x - 6, y - 6, x + 6, y + 6, width=1, outline='black', fill='yellow')
     
     def salir(self):
         self.master.quit()
