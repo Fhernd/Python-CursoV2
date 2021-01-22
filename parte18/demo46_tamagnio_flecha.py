1b8188024a059fbc0f0104221ae548ede3fcb250
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-22 13:44:50 -0500

    Python Curso V2: 701 2/2 Dibujar una Flecha Escalable desde un Componente Slider de Tkinter
    Dibujar una Flecha Escalable desde un Componente Slider de Tkinter con el lenguaje de programación Python.

--- parte18/demo46_tamagnio_flecha.py
+++ parte18/demo46_tamagnio_flecha.py
@@ -1,5 +1,4 @@
-from tkinter import Canvas, Scale, Tk
-import string
+from tkinter import Canvas, Scale, Tk, VERTICAL
 
 class Flecha:
 
@@ -13,6 +12,30 @@ class Flecha:
 
         canvas.create_polygon(0, 0, 1, 1, 2, 2, fill='blue', tags='flecha')
         canvas.create_line(0, 0, 1, 1, 2, 2, 0, 0, fill='black', tags='linea')
+
+        self.escala = Scale(self.master,
+                            orient=VERTICAL,
+                            length=285,
+                            from_=0,
+                            to=250,
+                            tickinterval=50,
+                            command=lambda h, c=canvas:establecerAltura(c, h))
+
+        self.escala.grid(row=0, column=0, sticky='NE')
+        canvas.grid(row=0, column=1, sticky='NWSE')
+
+        self.escala.set(100)
+
+def establecerAltura(canvas, altura):
+    altura = int(altura)
+    altura = altura + 21
+
+    y = altura - 30
+    if y < 21:
+        y = 21
+    
+    canvas.coords('flecha', 15, 20, 35, 20, 35, y, 45, y, 25, altura,5, y,15, y, 15, 20)
+    canvas.coords('linea', 15, 20, 35, 20, 35, y, 45, y, 25, altura, 5, y, 15, y, 15, 20)
 
 def main():
     app = Tk()
