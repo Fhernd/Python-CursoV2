b09b004d8835d5a1327f79efe3341753d887b9c0
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-23 13:17:08 -0500

    Python Curso V2: 703 2/2 Dibujar Óvalos Sobre un Lienzo (Canvas) Dispuesto en un Frame de Tkinter
    Dibujar Óvalos Sobre un Lienzo (Canvas) Dispuesto en un Frame de Tkinter con el lenguaje de programación Python.

--- parte18/demo47_ovalos.py
+++ parte18/demo47_ovalos.py
@@ -16,6 +16,13 @@ class Ovalos:
         canvas = Canvas(frm_canvas, bg='white', height=100, width=340)
         canvas.pack()
 
+        for i in range(25):
+            canvas.create_oval(5 + (4 * i), 5 + (3 * i), 6 * i + 60, i + 60, fill='yellow')
+
+        canvas.create_text(260, 80, text='Tkinter', font=('Arial', 11, 'bold'))
+
+        frm_canvas.pack(expand=1, fill=X, padx=5, pady=10)
+
 def main():
     master = Tk()
     master.title('Óvalos')
