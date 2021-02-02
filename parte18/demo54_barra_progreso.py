b46b504c42ace47608c2a26970ee3d5ecf3b2b5c
infzer0 <johnortizo@outlook.com>
2021-02-02 09:39:47 -0500

    Python Curso V2: 715 1/2 Usar el Componente Progressbar para Crear una Barra de Progreso en Tkinter
    Usar el Componente Progressbar para Crear una Barra de Progreso en Tkinter con el lenguaje de programación Python.

---
+++ parte18/demo54_barra_progreso.py
@@ -0,0 +1,25 @@
+from tkinter import Button, Tk
+from tkinter.ttk import Progressbar
+
+class Proceso:
+    def __init__(self, master):
+        self.master = master
+
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        pbr_proceso = Progressbar(self.master, orient='horizontal', mode='indeterminate', length=300)
+        pbr_proceso.grid(column=0, row=0, columnspan=2, padx=10, pady=10)
+
+
+def main():
+    master = Tk()
+    master.title('Proceso')
+    master.geometry('320x150')
+
+    proceso = Proceso(master)
+
+    master.mainloop()
+
+if __name__ == '__main__':
+    main()
