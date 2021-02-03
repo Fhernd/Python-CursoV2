0ce2994fe495d1eb369a9623e9b361019614b3db
infzer0 <johnortizo@outlook.com>
2021-02-03 10:01:13 -0500

    Python Curso V2: 718 2/2 Usar Componente OptionMenu para Mostrar Opciones de Selección en Tkinter
    Usar Componente OptionMenu para Mostrar Opciones de Selección en Tkinter con el lenguaje de programación Python.

--- parte18/demo55_menu_contextual_boton.py
+++ parte18/demo55_menu_contextual_boton.py
@@ -5,7 +5,7 @@ class Aplicacion(Tk):
         super().__init__()
 
         self.title('Uso de OptionMenu')
-        self.geometry('350x120')
+        self.geometry('390x120')
         self.lenguajes = ('Python', 'Java', 'Go', 'JavaScript', 'C++', 'C#', 'Swift')
 
         self.inicializar_gui()
@@ -15,6 +15,16 @@ class Aplicacion(Tk):
 
         lbl_seleccion_lenguaje = Label(self, text='Seleccione su lenguaje de programación favorito: ')
         lbl_seleccion_lenguaje.grid(column=0, row=0, sticky=W, padx=5, pady=5)
+
+        opm_lenguajes = OptionMenu(self, self.lenguaje_selecionado, self.lenguajes[0], *self.lenguajes, command=self.seleccionar_lenguaje)
+        
+        opm_lenguajes.grid(column=1, row=0, sticky=W, padx=5, pady=5)
+
+        self.lbl_lenguaje_seleccionado = Label(self, foreground='blue')
+        self.lbl_lenguaje_seleccionado.grid(column=0, row=1, sticky=W, padx=5, pady=5)
+    
+    def seleccionar_lenguaje(self, *args):
+        self.lbl_lenguaje_seleccionado['text'] = f'Ha seleccionado: {self.lenguaje_selecionado.get()}'
 
 def main():
     app = Aplicacion()
