c68b7c15d3a043d9fb400f897cfa37456f7a40fc
infzer0 <johnortizo@outlook.com>
2021-02-04 09:27:46 -0500

    Python Curso V2: 720 2/2 Crear una Aplicación para Cambiar la Apariencia de los Componentes de Tkinter
    Crear una Aplicación para Cambiar la Apariencia de los Componentes de Tkinter con el lenguaje de programación Python.

--- parte18/demo56_tema_colores.py
+++ parte18/demo56_tema_colores.py
@@ -1,4 +1,4 @@
-from tkinter import Entry, Label, LabelFrame, Radiobutton, StringVar, Tk
+from tkinter import Button, Entry, Label, LabelFrame, Radiobutton, StringVar, Tk
 from tkinter.ttk import Style
 
 class Apariencia(Tk):
@@ -14,9 +14,30 @@ class Apariencia(Tk):
 
         self.estilo = Style(self)
 
-        Label(self, text='Nombre').grid(column=0, row=0, padx=10, pady=10, sticky='w')
+        Label(self, text='Nombre:').grid(column=0, row=0, padx=10, pady=10, sticky='w')
 
         Entry(self).grid(column=1, row=0, padx=10, pady=10, sticky='w')
+
+        Button(self, text='Mostrar').grid(column=2, row=0, padx=10, pady=10, sticky='w')
+
+        self.tema_seleccionado = StringVar()
+
+        lbf_contenedor = LabelFrame(self, text='Temas')
+        lbf_contenedor.grid(ipadx=20, ipady=20, padx=10, pady=10, sticky='w')
+
+        for t in self.estilo.theme_names():
+            rbn_tema = Radiobutton(
+                lbf_contenedor,
+                text=t,
+                value=t,
+                variable=self.tema_seleccionado,
+                command=self.cambiar_tema
+            )
+
+            rbn_tema.pack(expand=True, fill='both')
+        
+    def cambiar_tema(self):
+        self.estilo.theme_use(self.tema_seleccionado.get())
 
 def main():
     app = Apariencia()
