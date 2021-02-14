d63c07106643d8af1fbabffef47c548cdc958077
infzer0 <johnortizo@outlook.com>
2021-02-14 12:58:59 -0500

    Python Curso V2: 726 2/5 Aplicación de Gestión de Comentarios desde una Interfaz Gráfica de Usuario
    Aplicación de Gestión de Comentarios desde una Interfaz Gráfica de Usuario con el lenguaje de programación Python.

--- parte18/demo59_app_comentarios.py
+++ parte18/demo59_app_comentarios.py
@@ -17,7 +17,19 @@ class ComentariosApp(Tk):
         self.estilo = ttk.Style()
         self.estilo.configure('TFrame', background='black')
         self.estilo.configure('TButton', background='#95A3B3')
-        self.estilo.configure('TLabel', background='#1E152A')
+        self.estilo.configure('TLabel', background='#1E152A', font=('Arial', 13))
+        self.estilo.configure('Encabezado.TLabel', font=('Arial', 18, 'bold'))
+
+        self.frm_encabezado = ttk.Frame(self)
+
+        self.img_logo = PhotoImage(file='parte18/python-logo.png')
+
+        lbl_logo = ttk.Label(self.frm_encabezado, image=self.img_logo)
+        lbl_logo.grid(column=0, row=0, rowspan=2)
+
+        lbl_nombre_app = ttk.Label(self.frm_encabezado, text='ComentariosApp', style='Encabezado.TLabel')
+        lbl_nombre_app.grid(column=1, row=0)
+
 
 def main():
     app = ComentariosApp()
