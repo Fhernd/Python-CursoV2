57cc4461f39b8eb0bca925bccbf8694d42405949
infzer0 <johnortizo@outlook.com>
2021-02-24 14:19:22 -0500

    Python Curso V2: 740 2/2 Control de Tipo Pestañas (Notebook) para Mostrar los 12 Meses del Año
    Control de Tipo Pestañas (Notebook) para Mostrar los 12 Meses del Año con el lenguaje de programación Python.

--- parte18/demo62_meses_tabs.py
+++ parte18/demo62_meses_tabs.py
@@ -10,13 +10,35 @@ class MesesTabs(Tk):
     
     def inicializar_gui(self):
         self.title('Meses del año')
-        self.geometry('580x100')
+        self.geometry('650x100')
 
         self.twd_meses = ttk.Notebook(self)
 
         tbs_meses = [ttk.Frame(self.twd_meses) for _ in range(12)]
-        meses = ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto'
+        meses = ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
                 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+        
+        for i in range(len(meses)):
+            print(meses[i])
+            self.twd_meses.add(tbs_meses[i], text=meses[i])
+        
+        frases_meses = ('Enero (Jano), dios de los portales',
+                        'Febrero (Febrau), mes de las hogueras purificatorias',
+                        'Marzo (Marte), dios de la guerra',
+                        'Abril (Aprilis), Apertura de las flores',
+                        'Mayo (Maia), diosa de la abundancia',
+                        'Junio (Juno), diosa del hogar y la familia',
+                        'Julio (Julio Cesar)',
+                        'Agosto (Octavio Augusto)',
+                        'Septiembre, Séptimo mes del año',
+                        'Octubre, Noveno mes del año',
+                        'Noviembre, Décimo mes del año',
+                        'Diciembre, Undécimo mes del año')
+        
+        for i in range(len(tbs_meses)):
+            Label(tbs_meses[i], text=frases_meses[i]).grid(column=8,row=8)
+        
+        self.twd_meses.pack(expand=1, fill='both')
 
 def main():
     app = MesesTabs()
