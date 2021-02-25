87fb911e5db20cd5bf56066053265301df87eb24
infzer0 <johnortizo@outlook.com>
2021-02-25 14:17:16 -0500

    Python Curso V2: 742 Usar la Librería requests para Obtener una Respuesta en Formato JSON
    Usar la Librería requests para Obtener una Respuesta en Formato JSON con el lenguaje de programación Python.

--- parte18/demo63_uso_basico_requests.py
+++ parte18/demo63_uso_basico_requests.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://randomuser.me/api?format=docx'
+url = 'https://randomuser.me/api?format=json'
 
 try:
     respuesta = requests.get(url)
@@ -10,14 +10,31 @@ try:
         
         print()
 
-        print('Contenido:')
+        print('Contenido como cadena de bytes:')
         print(type(respuesta.content))
         print(respuesta.content)
 
         print()
-        print('Contenido:')
+        print('Contenido en formato texto (str):')
         print(type(respuesta.text))
         print(respuesta.text)
+
+        print()
+
+        print('Contenido en formato JSON:')
+        contenido_json = respuesta.json()
+        print(type(contenido_json))
+        print(contenido_json)
+
+        print()
+
+        print('Contenido de results:')
+        print(type(contenido_json['results'])) # <class 'list'>
+        print(contenido_json['results'])
+        print(contenido_json['results'][0]['gender']) # ~
+        print(contenido_json['results'][0]['name']['first']) # ~
+
+        print()
     else:
         print('No se ha encontrado el recurso.')
 except:
