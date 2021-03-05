bde20ceb26b2a3ffece679a6910fe7d98603e921
infzer0 <johnortizo@outlook.com>
2021-03-05 10:48:35 -0500

    Python Curso V2: 749 2/6 Aplicación Tkinter Consulta de Datos de un Usuario en la Plataforma GitHub
    Aplicación Tkinter Consulta de Datos de un Usuario en la Plataforma GitHub con el lenguaje de programación Python.

--- parte18/demo65_github_usuario.py
+++ parte18/demo65_github_usuario.py
@@ -1,3 +1,26 @@
 import tkinter as tk
 import requests
 import PIL
+
+class GitHupUsuarioApp(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        self.title('GitHub Usuarios Consulta')
+
+        frm_entrada_datos = tk.Frame(self)
+        frm_entrada_datos.place(relx=0.5, rely=0.1, relheight=0.1, relwidth=0.75, anchor='n')
+
+        self.txt_usuario = tk.Entry(frm_entrada_datos, font=('Arial', 15))
+        self.txt_usuario.place(relx=0, rely=0, relheight=1, relwidth=0.65)
+
+def main():
+    app = GitHupUsuarioApp()
+    app.mainloop()
+
+if __name__ == '__main__':
+    main()
