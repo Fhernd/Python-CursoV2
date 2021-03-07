5651f5a27411bdb4778d8e561d102b19cbb8aca6
infzer0 <johnortizo@outlook.com>
2021-03-07 10:39:55 -0500

    Python Curso V2: 753 6/6 Aplicación Tkinter de Consulta de Datos de un Usuario en la Plataforma GitHub
    Aplicación Tkinter de Consulta de Datos de un Usuario en la Plataforma GitHub con el lenguaje de programación Python.

--- parte18/demo65_github_usuario.py
+++ parte18/demo65_github_usuario.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
+import urllib
 import requests
-import PIL
+import PIL.Image
+import PIL.ImageTk
 
 class GitHupUsuarioApp(tk.Tk):
 
@@ -26,50 +28,50 @@ class GitHupUsuarioApp(tk.Tk):
         btn_consultar.place(relx=0.7, rely=0, relheight=1, relwidth=0.3)
         btn_consultar['command'] = self.consultar
 
-        lbf_resultado = tk.LabelFrame(self, text='Resultados')
-        lbf_resultado.place(relx=0.5, rely=0.25, relheight=0.9, relwidth=0.9, anchor='n')
+        self.lbf_resultado = tk.LabelFrame(self, text='Resultados')
+        self.lbf_resultado.place(relx=0.5, rely=0.25, relheight=0.9, relwidth=0.9, anchor='n')
 
-        lbl_nombre = tk.Label(lbf_resultado, text='Nombre:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_nombre = tk.Label(self.lbf_resultado, text='Nombre:', width=12, justify=tk.LEFT, anchor='w')
         lbl_nombre.grid(row=0, column=0, padx=10, pady=2)
-        self.txt_nombre = tk.Entry(lbf_resultado, width=28)
+        self.txt_nombre = tk.Entry(self.lbf_resultado, width=28)
         self.txt_nombre.grid(row=0, column=1, padx=10, pady=2)
         
-        lbl_fecha_creacion = tk.Label(lbf_resultado, text='Fecha creación:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_fecha_creacion = tk.Label(self.lbf_resultado, text='Fecha creación:', width=12, justify=tk.LEFT, anchor='w')
         lbl_fecha_creacion.grid(row=1, column=0, padx=10, pady=2)
-        self.txt_fecha_creacion = tk.Entry(lbf_resultado, width=28)
+        self.txt_fecha_creacion = tk.Entry(self.lbf_resultado, width=28)
         self.txt_fecha_creacion.grid(row=1, column=1, padx=10, pady=2)
         
-        lbl_url = tk.Label(lbf_resultado, text='URL:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_url = tk.Label(self.lbf_resultado, text='URL:', width=12, justify=tk.LEFT, anchor='w')
         lbl_url.grid(row=2, column=0, padx=10, pady=2)
-        self.txt_url = tk.Entry(lbf_resultado, width=28)
+        self.txt_url = tk.Entry(self.lbf_resultado, width=28)
         self.txt_url.grid(row=2, column=1, padx=10, pady=2)
         
-        lbl_gists = tk.Label(lbf_resultado, text='URL Gists:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_gists = tk.Label(self.lbf_resultado, text='URL Gists:', width=12, justify=tk.LEFT, anchor='w')
         lbl_gists.grid(row=3, column=0, padx=10, pady=2)
-        self.txt_gists = tk.Entry(lbf_resultado, width=28)
+        self.txt_gists = tk.Entry(self.lbf_resultado, width=28)
         self.txt_gists.grid(row=3, column=1, padx=10, pady=2)
         
-        lbl_url_repos = tk.Label(lbf_resultado, text='URL repos:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_url_repos = tk.Label(self.lbf_resultado, text='URL repos:', width=12, justify=tk.LEFT, anchor='w')
         lbl_url_repos.grid(row=4, column=0, padx=10, pady=2)
-        self.txt_url_repos = tk.Entry(lbf_resultado, width=28)
+        self.txt_url_repos = tk.Entry(self.lbf_resultado, width=28)
         self.txt_url_repos.grid(row=4, column=1, padx=10, pady=2)
         
-        lbl_cantidad_repos = tk.Label(lbf_resultado, text='Cantidad repos:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_cantidad_repos = tk.Label(self.lbf_resultado, text='Cantidad repos:', width=12, justify=tk.LEFT, anchor='w')
         lbl_cantidad_repos.grid(row=5, column=0, padx=10, pady=2)
-        self.txt_cantidad_repos = tk.Entry(lbf_resultado, width=28)
+        self.txt_cantidad_repos = tk.Entry(self.lbf_resultado, width=28)
         self.txt_cantidad_repos.grid(row=5, column=1, padx=10, pady=2)
         
-        lbl_ubicacion = tk.Label(lbf_resultado, text='Ubicación:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_ubicacion = tk.Label(self.lbf_resultado, text='Ubicación:', width=12, justify=tk.LEFT, anchor='w')
         lbl_ubicacion.grid(row=6, column=0, padx=10, pady=2)
-        self.txt_ubicacion = tk.Entry(lbf_resultado, width=28)
+        self.txt_ubicacion = tk.Entry(self.lbf_resultado, width=28)
         self.txt_ubicacion.grid(row=6, column=1, padx=10, pady=2)
         
-        lbl_blog = tk.Label(lbf_resultado, text='Blog:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_blog = tk.Label(self.lbf_resultado, text='Blog:', width=12, justify=tk.LEFT, anchor='w')
         lbl_blog.grid(row=7, column=0, padx=10, pady=2)
-        self.txt_blog = tk.Entry(lbf_resultado, width=28)
+        self.txt_blog = tk.Entry(self.lbf_resultado, width=28)
         self.txt_blog.grid(row=7, column=1, padx=10, pady=2)
 
-        lbl_foto = tk.Label(lbf_resultado, text='Foto:', width=12, justify=tk.LEFT, anchor='w')
+        lbl_foto = tk.Label(self.lbf_resultado, text='Foto:', width=12, justify=tk.LEFT, anchor='w')
         lbl_foto.grid(row=8, column=0, padx=10, pady=2)
 
     def consultar(self):
@@ -96,8 +98,25 @@ class GitHupUsuarioApp(tk.Tk):
             self.txt_url.insert(0, respuesta_json['url'])
             self.txt_gists.delete(0, 'end')
             self.txt_gists.insert(0, respuesta_json['gists_url'])
-            # TODO...
-        except:
+            self.txt_url_repos.delete(0, 'end')
+            self.txt_url_repos.insert(0, respuesta_json['repos_url'])
+            self.txt_cantidad_repos.delete(0, 'end')
+            self.txt_cantidad_repos.insert(0, respuesta_json['public_repos'])
+            self.txt_ubicacion.delete(0, 'end')
+            self.txt_ubicacion.insert(0, respuesta_json['location'])
+            self.txt_blog.delete(0, 'end')
+            self.txt_blog.insert(0, respuesta_json['blog'])
+
+            urllib.request.urlretrieve(respuesta_json['avatar_url'], 'parte18/demo65_perfil.png')
+
+            img_perfil = PIL.Image.open('parte18/demo65_perfil.png')
+            img_perfil = img_perfil.resize((128, 128), PIL.Image.ANTIALIAS)
+            img_perfil = PIL.ImageTk.PhotoImage(img_perfil)
+            self.lbl_perfil = tk.Label(self.lbf_resultado, image=img_perfil)
+            self.lbl_perfil.image = img_perfil
+            self.lbl_perfil.grid(row=8, column=1)
+        except Exception as e:
+            print(e)
             messagebox.showwarning('Mensaje', 'El usuario especificado no existe en GitHub.')
 
 def main():
