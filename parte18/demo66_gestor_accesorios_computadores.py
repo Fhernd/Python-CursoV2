043ac44f403e2a809d1a91d018525f875887431d
infzer0 <johnortizo@outlook.com>
2021-03-10 17:09:26 -0500

    Python Curso V2: 756 3/7 App Tkinter para Gestión de Accesorios y Repuestos para Computador
    App Tkinter para Gestión de Accesorios y Repuestos para Computador con el lenguaje de programación Python.

--- parte18/demo66_gestor_accesorios_computadores.py
+++ parte18/demo66_gestor_accesorios_computadores.py
@@ -15,24 +15,37 @@ class GestorAccesoriosComputador(tk.Tk):
         self.geometry('550x340')
 
         frm_entrada = tk.Frame(self)
-        frm_entrada.place(relx=0.05, rely=0.05, relwidth=1, relheight=0.333333)
+        frm_entrada.place(relx=0.05, rely=0.05, relwidth=1, relheight=0.2, anchor='n')
 
         lbl_nombre = tk.Label(frm_entrada, text='Nombre:')
-        lbl_nombre.place(relx=0, rely=0, relwidth=0.25, relheight=1)
+        lbl_nombre.place(relx=0, rely=0, relwidth=0.25, relheight=0.5)
         self.txt_nombre = tk.Entry(frm_entrada)
         self.txt_nombre.place(relx=0.25, rely=0, relwidth=0.25, relheight=0.5)
         
         lbl_cliente = tk.Label(frm_entrada, text='Cliente:')
-        lbl_cliente.place(relx=0.5, rely=0, relwidth=0.25, relheight=1)
+        lbl_cliente.place(relx=0.5, rely=0, relwidth=0.25, relheight=0.5)
         self.txt_cliente = tk.Entry(frm_entrada)
         self.txt_cliente.place(relx=0.70, rely=0, relwidth=0.25, relheight=0.5)
         
         lbl_tipo = tk.Label(frm_entrada, text='Tipo:')
         lbl_tipo.place(relx=0, rely=0.5, relwidth=0.25, relheight=0.5)
         self.cbx_tipo = ttk.Combobox(frm_entrada)
-        self.cbx_tipo.place(relx=0.25, rely=0, relwidth=0.25, relheight=0.5)
+        self.cbx_tipo.place(relx=0.25, rely=0.5, relwidth=0.25, relheight=0.5)
 
+        lbl_costo = tk.Label(frm_entrada, text='Costo:')
+        lbl_costo.place(relx=0.5, rely=0.5, relwidth=0.25, relheight=0.5)
+        self.txt_costo = tk.Entry(frm_entrada)
+        self.txt_costo.place(relx=0.70, rely=0.5, relwidth=0.25, relheight=0.5)
 
+        frm_botones = tk.Frame(self)
+        frm_entrada.place(relx=0.05, rely=0.25, relwidth=1, relheight=0.1, anchor='n')
+
+        btn_agregar = tk.Button(frm_botones, text='Agregar')
+        btn_agregar['command'] = self.agregar
+        btn_agregar.place(relx=0, rely=0, relwidth=0.25, relheight=1)
+    
+    def agregar(self):
+        pass
 
 def main():
 
