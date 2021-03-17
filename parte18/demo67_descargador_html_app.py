30b4694f33d9a8eb4f4be81b9df49ea806ff8c44
infzer0 <johnortizo@outlook.com>
2021-03-17 14:30:12 -0500

    Python Curso V2: 763 3/8 Aplicación Tkinter para Descargar Contenido HTML Usando el Módulo threading
    Aplicación Tkinter para Descargar Contenido HTML Usando el Módulo threading con el lenguaje de programación Python.

--- parte18/demo67_descargador_html_app.py
+++ parte18/demo67_descargador_html_app.py
@@ -1,4 +1,24 @@
 import threading
 import tkinter as tk
+from tkinter import ttk
 import requests
 
+class DescargadorHtmlApp(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.inicializar_gui()
+
+    def inicializar_gui(self):
+        self.title('Descargador HTML')
+        self.geometry('680x420')
+        self.resizable(0, 0)
+
+        self.frm_superior = tk.Frame(self)
+        self.frm_superior.columnconfigure(0, weight=1)
+        self.frm_superior.columnconfigure(1, weight=10)
+        self.frm_superior.columnconfigure(2, weight=1)
+
+        lbl_url = ttk.Label(self.frm_superior, text='URL:')
+        lbl_url.grid(row=0, column=0, sticky=tk.W)
