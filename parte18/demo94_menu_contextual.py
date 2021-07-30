b134e7cc248e7f5f1ac871375aa80312a16a5793
infzer0 <johnortizo@outlook.com>
2021-07-30 16:48:50 -0500

    Python Curso V2: 815 2/3 GUI de Tkinter con Menú Contextual con Comandos de Edición de Texto
    GUI de Tkinter con Menú Contextual con Comandos de Edición de Texto con el lenguaje de programación Python.

--- parte18/demo94_menu_contextual.py
+++ parte18/demo94_menu_contextual.py
@@ -15,9 +15,33 @@ class MenuContextualApp(tk.Tk):
         self.mcx_edicion = tk.Menu(self, tearoff=0)
         self.mcx_edicion.add_command(label='Copiar', command=self.copiar)
         self.mcx_edicion.add_command(label='Cortar', command=self.cortar)
+        self.mcx_edicion.add_command(label='Pegar', command=self.pegar)
+        self.mcx_edicion.add_command(label='Eliminar', command=self.eliminar)
+
+        self.txa_contenido = tk.Text(self, height=12, width=80)
+        self.txa_contenido.bind('<Button-3>', self.mostrar_menu_contextual)
+        self.txa_contenido.pack()
     
+    def mostrar_menu_contextual(self, evento):
+        self.mcx_edicion.post(evento.x_root, evento.y_root)
+
     def copiar(self):
         pass
     
     def cortar(self):
         pass
+    
+    def pegar(self):
+        pass
+    
+    def eliminar(self):
+        pass
+
+
+def main():
+    app = MenuContextualApp()
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
