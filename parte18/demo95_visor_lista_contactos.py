bc566b941cdd1928cb70c2bfeecdd11e7de970c1
infzer0 <johnortizo@outlook.com>
2021-09-07 08:12:35 -0500

    Python Curso V2: 819 3/10 Creación Visor de Contactos Utilizando un Enfoque Compositivo para la GUI
    Creación Visor de Contactos Utilizando un Enfoque Compositivo para la GUI con el lenguaje de programación Python.

--- parte18/demo95_visor_lista_contactos.py
+++ parte18/demo95_visor_lista_contactos.py
@@ -1,5 +1,22 @@
 import re
 
+
+def dato_obligatorio(dato, mensaje):
+    if not dato:
+        raise ValueError(mensaje)
+    
+    return dato
+
+
+def coincide(dato, regex, mensaje):
+    if dato and not regex.match(dato):
+        raise ValueError(mensaje)
+    
+    if not dato:
+        dato_obligatorio(dato, mensaje)
+    
+    return dato
+
 class Contacto(object):
     regex_email = re.compile(r'[^@]+@[^@]+\.[^@]+')
     regex_telefono = re.compile(r'[1-9][0-9]{9}')
@@ -16,4 +33,12 @@ class Contacto(object):
     
     @apellido.setter
     def apellido(self, valor):
-        pass
+        self._apellido = dato_obligatorio(valor, 'El apellido es un campo obligatorio.')
+    
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, valor):
+        self._email = coincide(valor, self.regex_email, 'Formato inválido para el email.')
