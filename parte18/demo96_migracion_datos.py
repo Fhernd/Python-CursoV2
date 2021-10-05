e55a3ba748548a51f1fb191d732d352ab713ae83
infzer0 <johnortizo@outlook.com>
2021-10-05 16:22:05 -0500

    Python Curso V2: 827 1/3 Migrar los Datos de Contactos a una Base de Datos Tipo SQLite
    Migrar los Datos de Contactos a una Base de Datos Tipo SQLite con el lenguaje de programación Python.

---
+++ parte18/demo96_migracion_datos.py
@@ -0,0 +1,19 @@
+import csv
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+
+def main():
+    with open('parte18/demo95_contactos.csv') as f, \
+        sqlite3.connect('demo96_contactos.db') as conexion:
+
+        conexion.execute("""
+            CREATE TABLE contacto (
+                apellidos TEXT,
+                nombres TEXT,
+                email TEXT,
+                telefono TEXT
+            );
+        """)
+
+        conexion.executemany('INSERT INTO contacto VALUES (?, ?, ?, ?)', csv.reader(f))
