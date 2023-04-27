5cad44f15204c67a7c33e0c795591b40b8cbefc8
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-27 11:27:48 -0500

    Primera parte de la demo 96 de Tkinter: descargador de HTML

---
+++ parte18/demo97_descargador_html.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from urllib.request import urlopen
+
+
+class DescargarHTML(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        
+        self.inicializar_gui()
+    
+    def inicializar_gui(self):
+        self.lbl_url = tk.Label(self, text='Dirección URL:')
+        self.lbl_url.pack(side='top')
+
+        self.txt_url = tk.Entry(self)
+        self.txt_url.pack(side='top')
+
+        self.btn_descargar = tk.Button(self, text='Descargar HTML', command=self.descargarHtml)
+        self.btn_descargar.pack(side='bottom')
+
+        self.txa_contenido_html = tk.Text(self, height=20, width=20)
+        self.txa_contenido_html.pack(side='bottom')
+
