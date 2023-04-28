025a7163481ea8e3b09578d9234335a2e90b03b9
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-28 14:38:17 -0500

    Definición del inicializador de la ventana principal

--- parte18/demo99_lectura_datos_api.py
+++ parte18/demo99_lectura_datos_api.py
@@ -2,3 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 
 import requests
+
+
+class LecturaDatosApi(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.master = master
+
+        self.inicializar_gui()
+    
+    
