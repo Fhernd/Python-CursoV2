d7895510c4e50c39f2eaa19ab60e241cacea9590
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-20 15:16:21 -0500

    Función para consultar el contenido de una URL

---
+++ parte19_proyecto1_scraping/main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def consultar_url(url):
+    """
+    Función que consulta una URL y devuelve el contenido de la respuesta.
+
+    :param url: URL a consultar.
+    :return: Contenido de la respuesta.
+    """
+    response = requests.get(url)
+    
+    return response.content
