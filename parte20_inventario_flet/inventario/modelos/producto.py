4ee57ff745dd638e93f7bc4b24253ac8c22e8fd7
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-25 09:47:22 -0500

    Clase producto con documentación

--- parte20_inventario_flet/inventario/modelos/producto.py
+++ parte20_inventario_flet/inventario/modelos/producto.py
@@ -1,5 +1,19 @@
 class Producto:
+    """
+    Representa un producto del inventario.
+    """
     def __init__(self, codigo, nombre, precio, cantidad, disponible):
+        """
+        Constructor de la clase.
+
+        Parameters:
+        codigo: Código del producto.
+        nombre: Nombre del producto.
+        precio: Precio del producto.
+        cantidad: Cantidad del producto.
+        disponible: ¿Está disponible el producto?
+        """
+        
         self.codigo = codigo
         self.nombre = nombre
         self.precio = precio
@@ -7,4 +21,10 @@ class Producto:
         self.disponible = disponible
     
     def __str__(self):
+        """
+        Representación en cadena de texto del producto.
+
+        Returns:
+        Representación en cadena de texto del producto.
+        """
         return f'ID: {self.codigo}\nNombre: {self.nombre}\nPrecio: {self.precio}\nCantidad: {self.cantidad}\n¿Disponible?: {"Sí" if self.disponible else "No"}'
