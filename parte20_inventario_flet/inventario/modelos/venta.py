7cf6113c4a40987748d9ed080e3285d9f207d1e3
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-25 09:48:06 -0500

    Documentación de la clase venta

--- parte20_inventario_flet/inventario/modelos/venta.py
+++ parte20_inventario_flet/inventario/modelos/venta.py
@@ -1,7 +1,19 @@
 from datetime import datetime
 
 class Venta:
+    """
+    Representa una venta realizada.
+    """
     def __init__(self, codigo_producto, cantidad, total_sin_iva, fecha=datetime.now()):
+        """
+        Constructor de la clase.
+
+        Parameters:
+        codigo_producto: Código del producto.
+        fecha: Fecha de la venta.
+        cantidad: Cantidad del producto.
+        total_sin_iva: Total sin IVA de la venta.
+        """
         self.codigo_producto = codigo_producto
         self.fecha = fecha
         self.cantidad = cantidad
